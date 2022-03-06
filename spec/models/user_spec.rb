570582d735cc368adf44724afc878c227572c2cc
Daniel Calvin <arockne2003@yahoo.com>
2022-03-06 11:58:43 -0700

    update testing empty username to include other attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,7 +30,15 @@ RSpec.describe User, type: :model do
         expect { User.create!() }.to raise_exception ActiveRecord::RecordInvalid
       end
       it "raises exception when username is empty" do
-        expect { User.create!(username: '') }.to raise_exception ActiveRecord::RecordInvalid
+        expect do 
+          User.create!(
+            username: '', 
+            email: 'bob123@bob.com', 
+            email_confirmation: 'bob123@bob.com',
+            password: 'test123', 
+            password_confirmation: 'test123'
+          )
+        end.to raise_exception ActiveRecord::RecordInvalid
       end
       it "returns false when creating a duplicate user" do
         user.save

52e40e50cdfc4b4174a8574824cc1466934139a6
Daniel Calvin <arockne2003@yahoo.com>
2022-03-10 16:40:22 -0700

    add POST /api/login

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     post '/signup', to: 'users#create'
+    post '/login', to: 'sessions#create'
   end
 end

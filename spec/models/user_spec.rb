c24c79d248e38278fc7f2063b5ca1f6915bb7f2c
Daniel Calvin <arockne2003@yahoo.com>
2022-03-09 19:33:32 -0700

    add attribute tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  describe 'attributes' do
+    it { is_expected.to respond_to(:username) }
+    it { is_expected.to respond_to(:email) }
+    it { is_expected.to respond_to(:password_digest) }
+  end
 end

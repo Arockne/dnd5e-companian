3efd6eed9c094526b632cd1a62c8df0a4e6b1ef6
Daniel Calvin <arockne2003@yahoo.com>
2022-03-10 14:33:19 -0700

    add validation presence to password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, confirmation: true
   validates :email_confirmation, presence: true
+  validates :password_confirmation, presence: true
   has_secure_password
 end

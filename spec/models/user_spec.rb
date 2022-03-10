a360222838931bc4a43f9e9d0936c51ad09d11b0
Daniel Calvin <arockne2003@yahoo.com>
2022-03-10 14:32:46 -0700

    test presence of password_confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,12 +24,13 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { is_expected.to have_secure_password }
+    it { is_expected.to validate_presence_of(:password_confirmation) }
     it { is_expected.to validate_presence_of(:username)}
     it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
     it { is_expected.to validate_length_of(:username).is_at_least(3)}
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     it { is_expected.to validate_confirmation_of(:email) }
-    it { is_expected.to validate_presence_of(:email_confirmation)}
+    it { is_expected.to validate_presence_of(:email_confirmation) }
   end
 end

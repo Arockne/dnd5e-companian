507d79cb764cc92c600a023faa603d1e14aa251d
Daniel Calvin <arockne2003@yahoo.com>
2022-03-10 16:29:24 -0700

    test invalid password on login

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -31,8 +31,25 @@ RSpec.describe "Sessions", type: :request do
       it 'returns a status code of 200 (ok)' do
         expect { post '/api/login', params: session_params }.to have_http_status(:ok)
       end
-
     end
 
+    context 'with invalid password' do
+      let(:session_params) { { username: user.username, password: 'testabc' } }
+
+      it 'returns the error messages' do
+        post '/api/login', params: session_params
+
+        expect(response.body).to include_json({
+          errors: a_kind_of(Array)
+        })
+      end
+
+      it 'returns a status code of 401(Unauthorized)' do
+        post '/api/login', params: session_params
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+    
   end
 end

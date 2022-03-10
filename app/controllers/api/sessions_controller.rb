deb8bef047ceb3bdfb6c6aadf71e5d21295b46f4
Daniel Calvin <arockne2003@yahoo.com>
2022-03-10 16:59:16 -0700

    implement logging in

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,12 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    user = User.where('lower(username) = ?', params[:username]).first
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+    end
+  end
 end

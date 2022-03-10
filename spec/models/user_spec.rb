8fc8231450b770cdd74edc9c702f2ecd9cb2bf2c
Daniel Calvin <arockne2003@yahoo.com>
2022-03-09 19:42:06 -0700

    add test for username validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe User, type: :model do
     it { is_expected.to respond_to(:email) }
     it { is_expected.to respond_to(:password_digest) }
   end
+
+  describe 'validations' do
+    it { is_expected.to have_secure_password }
+    it { is_expected.to validate_presence_of(:username)}
+    it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
+  end
 end

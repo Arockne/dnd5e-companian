90ac22072ff0e9522f12190098b86b5027208d1b
Daniel Calvin <arockne2003@yahoo.com>
2022-03-11 10:11:39 -0700

    test attributes

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Campaign, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'attributes' do
+    it { is_expected.to respond_to(:owner) }
+    it { is_expected.to respond_to(:name) }
+    it { is_expected.to respond_to(:setting) }
+  end
+
 end

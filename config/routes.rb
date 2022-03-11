805d48089c46ffa314c85a9061da310af169787c
Daniel Calvin <arockne2003@yahoo.com>
2022-03-11 09:27:29 -0700

    change HTTP verb from POST to DELETE on '/logout'

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   namespace :api do
     post '/signup', to: 'users#create'
     post '/login', to: 'sessions#create'
-    post '/logout', to: 'sessions#destroy'
+    delete '/logout', to: 'sessions#destroy'
   end
 end

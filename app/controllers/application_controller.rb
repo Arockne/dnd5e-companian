dcaf16c7283170752885d468225eddb69d88b883
Daniel Calvin <arockne2003@yahoo.com>
2022-03-11 09:05:24 -0700

    implement authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+
+  before_action :authorize
+
+  def current_user
+    @current_user ||= session[:user_id] && User.find(session[:user_id])
+  end
+  
+  def authorize
+    render json: { errors: ['Not authorized'] }, status: :unauthorized unless current_user
+  end
 end

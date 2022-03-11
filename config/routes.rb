d997fbd3099454bb8c6dc7fc66e0e1de2e9dd187
Daniel Calvin <arockne2003@yahoo.com>
2022-03-11 07:02:53 -0700

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   namespace :api do
     post '/signup', to: 'users#create'
     post '/login', to: 'sessions#create'
+    post '/logout', to: 'sessions#destroy'
   end
 end

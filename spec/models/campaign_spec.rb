73ef47c5b537059828ea067f04c9548b3f065f55
Daniel Calvin <arockne2003@yahoo.com>
2022-03-11 14:09:01 -0700

    add validation tests

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Campaign, type: :model do
-  
+
   describe 'attributes' do
     it { is_expected.to respond_to(:owner) }
     it { is_expected.to respond_to(:name) }
     it { is_expected.to respond_to(:setting) }
+    it { is_expected.to respond_to(:password_digest) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to have_secure_password }
+    it { is_expected.to validate_presence_of(:password_confirmation) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(30) }
   end
 
 end

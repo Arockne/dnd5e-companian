e6a9c2282255eded08e9376c9d29469157dfb270
Daniel Calvin <arockne2003@yahoo.com>
2022-03-11 14:09:25 -0700

    add validation

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,7 @@
 class Campaign < ApplicationRecord
-  belongs_to :owner, class_name: 'User'
+  belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :password_confirmation, presence: true
+  has_secure_password
 end

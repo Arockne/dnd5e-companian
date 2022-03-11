87d9e0963093eaae9ec1234f238839b228d57dc1
Daniel Calvin <arockne2003@yahoo.com>
2022-03-11 09:55:06 -0700

    add campaign model generated files

---
+++ db/migrate/20220311165424_create_campaigns.rb
@@ -0,0 +1,11 @@
+class CreateCampaigns < ActiveRecord::Migration[6.1]
+  def change
+    create_table :campaigns do |t|
+      t.string :name
+      t.string :string
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

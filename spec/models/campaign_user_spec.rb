46db277ab74c0cd940e7ab14f1e60a941842f160
Daniel Calvin <arockne2003@yahoo.com>
2022-03-12 12:20:25 -0700

    test attributes

--- spec/models/campaign_user_spec.rb
+++ spec/models/campaign_user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CampaignUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'attributes' do
+    it { is_expected.to respond_to(:user) }
+    it { is_expected.to respond_to(:campaign) }
+  end
+  
 end

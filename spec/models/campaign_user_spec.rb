9d5651c5e1da657037136625845f5f035d8e2d96
Daniel Calvin <arockne2003@yahoo.com>
2022-03-12 14:13:11 -0700

    reorganize

--- spec/models/campaign_user_spec.rb
+++ spec/models/campaign_user_spec.rb
@@ -1,6 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe CampaignUser, type: :model do
+  let(:campaign_owner) do
+    User.create!(
+      username: 'arthur',
+      email: 'arthur@camelot.com',
+      email_confirmation: 'arthur@camelot.com', 
+      password: 'test123', 
+      password_confirmation: 'test123'
+    )
+  end
+  
+  let(:other_user) do
+    User.create!(
+      username: 'bob',
+      email: 'bob@gmail.com',
+      email_confirmation: 'bob@gmail.com', 
+      password: 'test123', 
+      password_confirmation: 'test123'
+    )
+  end
+
+  let(:campaign) do
+    Campaign.create!(
+      name: 'Knights of the Round Table', 
+      setting: 'Somewhere in Camelot', 
+      owner: campaign_owner, 
+      password: 'king', 
+      password_confirmation: 'king'
+    )
+  end
+
+  subject(:user_campaign) { CampaignUser.create(user: other_user, campaign: campaign)}
   
   describe 'attributes' do
     it { is_expected.to respond_to(:user) }
@@ -8,35 +39,6 @@ RSpec.describe CampaignUser, type: :model do
   end
 
   describe 'validations' do
-    let(:campaign_owner) do
-      User.create!(
-        username: 'arthur',
-        email: 'arthur@camelot.com',
-        email_confirmation: 'arthur@camelot.com', 
-        password: 'test123', 
-        password_confirmation: 'test123'
-      )
-    end
-    let(:other_user) do
-      User.create!(
-        username: 'bob',
-        email: 'bob@gmail.com',
-        email_confirmation: 'bob@gmail.com', 
-        password: 'test123', 
-        password_confirmation: 'test123'
-      )
-    end
-    let(:campaign) do
-      Campaign.create!(
-        name: 'Knights of the Round Table', 
-        setting: 'Somewhere in Camelot', 
-        owner: campaign_owner, 
-        password: 'king', 
-        password_confirmation: 'king'
-      )
-    end
-    subject(:user_campaign) { CampaignUser.create(user: other_user, campaign: campaign)}
-      
     it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:campaign_id)}
   end
 

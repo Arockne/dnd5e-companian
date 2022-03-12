cda2ff988a7526233134b674d469550bde61c90a
Daniel Calvin <arockne2003@yahoo.com>
2022-03-12 11:09:37 -0700

    implement create action

--- app/controllers/api/campaigns_controller.rb
+++ app/controllers/api/campaigns_controller.rb
@@ -4,4 +4,19 @@ class Api::CampaignsController < ApplicationController
     campaigns = Campaign.all
     render json: campaigns, status: :ok
   end
+
+  def create
+    campaign = current_user.campaigns.new(campaign_params)
+    if campaign.save
+      render json: campaign, status: :created
+    else
+      render json: { errors: campaign.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def campaign_params
+    params.require(:campaign).permit(:name, :setting, :password, :password_confirmation)
+  end
 end

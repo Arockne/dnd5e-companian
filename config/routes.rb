a2a8da349555b984a1ed3695db639dd994acf95c
Daniel Calvin <arockne2003@yahoo.com>
2022-03-12 10:58:27 -0700

    add campaigns#create route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     post '/signup', to: 'users#create'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
-    resources :campaigns, only: [:index]
+    resources :campaigns, only: [:index, :create]
   end
 end

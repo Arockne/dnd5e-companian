85021e681619006ba440d34e317a6b3eb7d4ee25
Daniel Calvin <arockne2003@yahoo.com>
2022-03-12 09:16:42 -0700

    add route to campaigns index action

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     post '/signup', to: 'users#create'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
+    resources :campaigns, only: [:index]
   end
 end

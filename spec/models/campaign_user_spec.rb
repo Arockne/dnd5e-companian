7572586cc5716b056f1f9ee221218bfebab0a3b3
Daniel Calvin <arockne2003@yahoo.com>
2022-03-12 12:59:36 -0700

    test uniqueness constraint between a user_id and campaign_id

--- spec/models/campaign_user_spec.rb
+++ spec/models/campaign_user_spec.rb
@@ -6,5 +6,38 @@ RSpec.describe CampaignUser, type: :model do
     it { is_expected.to respond_to(:user) }
     it { is_expected.to respond_to(:campaign) }
   end
-  
+
+  describe 'validations' do
+    let(:campaign_owner) do
+      User.create!(
+        username: 'arthur',
+        email: 'arthur@camelot.com',
+        email_confirmation: 'arthur@camelot.com', 
+        password: 'test123', 
+        password_confirmation: 'test123'
+      )
+    end
+    let(:other_user) do
+      User.create!(
+        username: 'bob',
+        email: 'bob@gmail.com',
+        email_confirmation: 'bob@gmail.com', 
+        password: 'test123', 
+        password_confirmation: 'test123'
+      )
+    end
+    let(:campaign) do
+      Campaign.create!(
+        name: 'Knights of the Round Table', 
+        setting: 'Somewhere in Camelot', 
+        owner: campaign_owner, 
+        password: 'king', 
+        password_confirmation: 'king'
+      )
+    end
+    subject(:user_campaign) { CampaignUser.create(user_id: other_user.id, campaign_id: campaign.id)}
+      
+    it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:campaign_id)}
+  end
+
 end

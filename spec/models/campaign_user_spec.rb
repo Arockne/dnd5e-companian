66e11b04314210b698e18bf5ebeeb20fb98fca99
Daniel Calvin <arockne2003@yahoo.com>
2022-03-12 13:55:41 -0700

    test for user and campaign associations

--- spec/models/campaign_user_spec.rb
+++ spec/models/campaign_user_spec.rb
@@ -40,4 +40,9 @@ RSpec.describe CampaignUser, type: :model do
     it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:campaign_id)}
   end
 
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:campaign) }
+  end
+
 end

d0ccfa81ee513091ff1b4777445f958d44b6739c
Daniel Calvin <arockne2003@yahoo.com>
2022-03-12 07:25:37 -0700

    add has many relationship with campaigns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :campaigns
+
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, confirmation: true
   validates :email_confirmation, presence: true

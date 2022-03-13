a59fdde6209a6e84d63eccf2db6ff67655fa7164
Daniel Calvin <arockne2003@yahoo.com>
2022-03-13 08:41:31 -0700

    add associations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,7 @@
 class Campaign < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
+  has_many :campaign_users
+  has_many :users, through: :campaign_users
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :password_confirmation, presence: true

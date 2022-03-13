a0afc1effdce6ac40243045064d8fdfb8d6c86e6
Daniel Calvin <arockne2003@yahoo.com>
2022-03-13 10:59:43 -0700

    add destroy action to campaigns

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     post '/signup', to: 'users#create'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
-    resources :campaigns, only: [:index, :create]
+    resources :campaigns, only: [:index, :create, :destroy]
   end
 end

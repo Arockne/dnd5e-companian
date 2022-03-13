eed91b504bfe90d05a1c456c88d16709323b0cf4
Daniel Calvin <arockne2003@yahoo.com>
2022-03-13 08:36:10 -0700

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :campaigns
+  has_many :owned_campaigns, class_name: 'Campaign'
+  has_many :campaign_users
+  has_many :campaigns, through: :campaign_users
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, confirmation: true

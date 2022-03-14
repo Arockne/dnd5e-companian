9dcf4d9392456961857a1ce2648529187e08030a
Daniel Calvin <arockne2003@yahoo.com>
2022-03-13 20:28:38 -0700

    implement test descriptions

--- spec/requests/api/campaign_users_spec.rb
+++ spec/requests/api/campaign_users_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Api::CampaignUsers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'POST /create' do
+    context 'with logged in user' do
+      context 'joining a campaign' do
+        context 'with correct password' do
+          it 'increases the amount of CampaignUsers by one'
+          it 'returns the CampaignUser'
+          it 'returns a status of 201 (Created)'
+        end
+        context 'with incorrect password' do
+          it 'returns a status of 401 (Unauthorized)'
+          it 'returns error messsages'
+        end
+      end
+      context 'joining a campaign already a member of' do
+        it 'returns a status of 403 (Forbidden)'
+        it 'returns error messages'
+      end
+    end
+    context 'without logged in user' do
+      it 'returns a status of 401 (Unauthorized)'
+      it 'returns error messages'
+    end
   end
 end

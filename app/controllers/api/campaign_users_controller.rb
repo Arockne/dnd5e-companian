98f8c42186b152782dc16d848aac9fa19a11f226
Daniel Calvin <arockne2003@yahoo.com>
2022-03-14 13:29:39 -0700

    refactor with authorize_join_request

--- app/controllers/api/campaign_users_controller.rb
+++ app/controllers/api/campaign_users_controller.rb
@@ -2,13 +2,11 @@ class Api::CampaignUsersController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 rescue_from ActiveRecord::RecordInvalid, with: :render_foribidden
 
+  before_action :authorize_join_request, only: [:create]
+
   def create
-    if campaign&.authenticate(campaign_user_params[:password])
-      campaign_user = current_user.campaign_users.create!(campaign_id: campaign_user_params[:campaign_id])
-      render json: campaign_user, status: :created
-    else
-      render json: { errors: ['Not Authorized'] }, status: :unauthorized
-    end
+    campaign_user = current_user.campaign_users.create!(campaign_id: campaign_user_params[:campaign_id])
+    render json: campaign_user, status: :created
   end
 
   private
@@ -19,6 +17,14 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_foribidden
 
   def campaign
     @campaign ||= Campaign.find(campaign_user_params[:campaign_id])
+  end
+
+  def authenticate_join_request
+    campaign&.authenticate(campaign_user_params[:password])
+  end
+
+  def authorize_join_request
+    render json: { errors: ['Not Authorized'] }, status: :unauthorized unless authenticate_join_request
   end
 
   def render_not_found

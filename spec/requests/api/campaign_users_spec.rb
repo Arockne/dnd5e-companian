3915756a9c192dbfd39250f04e25f8dd5f6b5bb7
Daniel Calvin <arockne2003@yahoo.com>
2022-03-14 11:14:17 -0700

    test when a user joins a campaign already a member of

--- spec/requests/api/campaign_users_spec.rb
+++ spec/requests/api/campaign_users_spec.rb
@@ -84,8 +84,19 @@ RSpec.describe "Api::CampaignUsers", type: :request do
         end
       end
       context 'joining a campaign already a member of' do
-        it 'returns a status of 403 (Forbidden)'
-        it 'returns error messages'
+        let(:campaign_user_params) { { campaign: { campaign_id: campaign_1.id, password: campaign_1.password } } }
+        it 'returns a status of 403 (Forbidden)' do
+          post '/api/campaign_users', params: campaign_user_params
+          post '/api/campaign_users', params: campaign_user_params
+          expect(response).to have_http_status(:forbidden)
+        end
+        it 'returns error messages' do
+          post '/api/campaign_users', params: campaign_user_params
+          post '/api/campaign_users', params: campaign_user_params
+          expect(response.body).to include_json({
+            errors: a_kind_of(Array)
+          })
+        end
       end
     end
     context 'without logged in user' do

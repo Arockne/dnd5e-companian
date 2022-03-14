a50d6bc7e8544963ad2f2e2c60c28f1b0d4b2022
Daniel Calvin <arockne2003@yahoo.com>
2022-03-14 13:14:24 -0700

    refactor create with render_forbidden

--- app/controllers/api/campaign_users_controller.rb
+++ app/controllers/api/campaign_users_controller.rb
@@ -1,17 +1,12 @@
 class Api::CampaignUsersController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_foribidden
 
   def create
     campaign = Campaign.find(campaign_user_params[:campaign_id])
     if campaign&.authenticate(campaign_user_params[:password])
-      campaign_user = current_user.campaign_users.new(campaign_id: campaign_user_params[:campaign_id])
-      if campaign_user.save
-        render json: campaign_user, status: :created
-      else
-        render json: { errors: campaign_user.errors.full_messages }, status: :forbidden
-      end
-    elsif campaign.nil?
-      render json: { errors: ['Campaign does not exist'] }, status: :not_found
+      campaign_user = current_user.campaign_users.create!(campaign_id: campaign_user_params[:campaign_id])
+      render json: campaign_user, status: :created
     else
       render json: { errors: ['Not Authorized'] }, status: :unauthorized
     end
@@ -25,5 +20,9 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def render_not_found
     render json: { errors: ['Campaign does not exist'] }, status: :not_found
+  end
+
+  def render_foribidden(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :forbidden
   end
 end

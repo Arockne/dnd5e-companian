db3ef4c4339647f3c02cbe7c397f06bdc1f5e6d1
Daniel Calvin <arockne2003@yahoo.com>
2022-03-15 15:32:38 -0700

    implement attribute tests

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Character, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'attributes' do
+    it { is_expected.to respond_to(:name) }
+    it { is_expected.to respond_to(:background) }
+    it { is_expected.to respond_to(:race) }
+    it { is_expected.to respond_to(:profession) }
+    it { is_expected.to respond_to(:alignment) }
+    it { is_expected.to respond_to(:experience) }
+    it { is_expected.to respond_to(:strength) }
+    it { is_expected.to respond_to(:dexterity) }
+    it { is_expected.to respond_to(:constitution) }
+    it { is_expected.to respond_to(:intelligence) }
+    it { is_expected.to respond_to(:wisdom) }
+    it { is_expected.to respond_to(:charisma) }
+    it { is_expected.to respond_to(:campaign_user_id) }
+    it { is_expected.to respond_to(:campaign_id) }
+  end
+
 end

972a3ec10994c15bd1cc08167f05b71a089c3a4b
Daniel Calvin <arockne2003@yahoo.com>
2022-03-17 13:12:34 -0700

    add test descriptions for getting users characters

--- spec/requests/api/character_spec.rb
+++ spec/requests/api/character_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Api::Characters", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    context 'when a user is logged in' do
+      it 'returns the user\'s characters'
+      it 'has a status of 200 (Ok)'
+    end
+    context 'when a user is logged out' do
+      it 'returns the error messages'
+      it 'returns a status of 401 (Unauthorized)'
+    end
   end
 end

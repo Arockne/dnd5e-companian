d0da6081141a4e94ec0bf376c60e88172748b8f5
Daniel Calvin <arockne2003@yahoo.com>
2022-03-17 06:20:20 -0700

    add validations

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,10 @@
 class Character < ApplicationRecord
   belongs_to :campaign_user
   belongs_to :campaign
+
+  validates :name, presence: true
+  validates :background, presence: true
+  validates :race, presence: true
+  validates :profession, presence: true
+  validates :alignment, inclusion: { in: [ 'Lawful good', 'Neutral good', 'Chaotic good', 'Lawful neutral', 'Neutral', 'Chaotice neutral', 'Lawful evil', 'Neutral evil', 'Chaotic evil' ]}
 end

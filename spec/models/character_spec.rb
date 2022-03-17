0af5cf8dddbee8d04ea83226a1888810eb2231df
Daniel Calvin <arockne2003@yahoo.com>
2022-03-17 06:04:33 -0700

    test associations

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -25,24 +25,22 @@ RSpec.describe Character, type: :model do
     it { is_expected.to validate_presence_of(:race) }
     it { is_expected.to validate_presence_of(:profession) }
     
-    it { is_expected.to validate_inclusion_of(:alignment).in_array([
-      'Lawful good',
-      'Neutral good',
-      'Chaotic good',
-      'Lawful neutral',
-      'Neutral',
-      'Chaotice neutral'
-      'Lawful evil',
-      'Neutral evil',
-      'Chaotic evil'
-    ])}
+    it do 
+      is_expected.to validate_inclusion_of(:alignment).
+      in_array([ 'Lawful good', 'Neutral good', 'Chaotic good', 'Lawful neutral', 'Neutral', 'Chaotice neutral', 'Lawful evil', 'Neutral evil', 'Chaotic evil' ])
+    end
 
     it { is_expected.to validate_numericality_of(:experience).is_greater_than(0) }
-    
+
     [:strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma].each do |ability_score|
       it { is_expected.to validate_numericality_of(ability_score).is_greater_than(0) }
     end
 
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:campaign_user) }
+    it { is_expected.to belong_to(:campaign) }
   end
 
 end

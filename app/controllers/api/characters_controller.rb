e4f1fdb810e503cd0fbbef2f86840eb5522f6c46
Daniel Calvin <arockne2003@yahoo.com>
2022-03-18 06:17:17 -0700

    implement create action

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -6,13 +6,24 @@ class Api::CharactersController < ApplicationController
   end
 
   def create
-    
+    membership = current_user.campaign_users.find_by(campaign_id: character_params[:campaign_id])
+    owner = membership.nil? && current_user.owned_campaigns.find_by(id: character_params[:campaign_id])
+    if membership || owner
+      character = current_user.characters.new(character_params)
+      if character.save
+        render json: character, status: :created, serializer: CharacterShowSerializer
+      else
+        render json: { errors: character.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { errors: ['Not Authorized'] }, status: :unauthorized
+    end
   end
 
   private
 
   def character_params
-    params.require(:character).permit(:name, :background, :race, :profession, :alignment, :experience, :strength, :dexterity, :constitution, :intelligence, :wisdom, :image_url)
+    params.require(:character).permit(:name, :background, :race, :profession, :alignment, :experience, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :image_url, :campaign_id)
   end
 
 end

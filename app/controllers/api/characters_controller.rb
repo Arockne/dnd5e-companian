ea345a422f1b67de645b74b5b12b4460b576411a
Daniel Calvin <arockne2003@yahoo.com>
2022-03-17 19:23:57 -0700

    implement character_params

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -4,4 +4,15 @@ class Api::CharactersController < ApplicationController
     characters = current_user.characters
     render json: characters, status: :ok
   end
+
+  def create
+    
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :background, :race, :profession, :alignment, :experience, :strength, :dexterity, :constitution, :intelligence, :wisdom, :image_url)
+  end
+
 end

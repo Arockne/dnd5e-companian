fe1d9bd7123bd6d90963a9565257fd1d2d427d50
Daniel Calvin <arockne2003@yahoo.com>
2022-03-18 11:08:48 -0700

    refactor create action with campaign and owner? method

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -6,8 +6,7 @@ class Api::CharactersController < ApplicationController
   end
 
   def create
-    owner = membership.nil? && current_user.owned_campaigns.find_by(id: character_params[:campaign_id])
-    if membership || owner
+    if membership || owner?
       character = current_user.characters.new(character_params)
       if character.save
         render json: character, status: :created, serializer: CharacterShowSerializer
@@ -24,9 +23,17 @@ class Api::CharactersController < ApplicationController
   def character_params
     params.require(:character).permit(:name, :background, :race, :profession, :alignment, :experience, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :image_url, :campaign_id)
   end
+  
+  def campaign
+    @campaign ||= character_params[:campaign_id] && Campaign.find_by(id: character_params[:campaign_id])
+  end
 
   def membership
-    @membership ||= current_user.campaign_users.find_by(campaign_id: character_params[:campaign_id])
+    @membership ||= current_user.campaign_users.find_by(campaign: campaign)
+  end
+
+  def owner?
+    campaign.owner == current_user unless campaign.nil?
   end
 
 end

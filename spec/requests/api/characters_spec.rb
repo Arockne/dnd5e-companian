092c2d4b5f8a41c335612c0b9215840d22b7f688
Daniel Calvin <arockne2003@yahoo.com>
2022-03-19 16:02:45 -0700

    add test to GET /show to test when a user is not affiliated to the campaign and the user has their character visible

--- spec/requests/api/characters_spec.rb
+++ spec/requests/api/characters_spec.rb
@@ -340,7 +340,7 @@ RSpec.describe "Api::Characters", type: :request do
             expect(response).to have_http_status(:ok)
           end
         end
-        
+
         context 'the character does not allow visibility' do
           it 'returns the error messages' do
             get "/api/campaigns/#{invisible_character.campaign_id}/characters/#{invisible_character.id}"
@@ -357,8 +357,17 @@ RSpec.describe "Api::Characters", type: :request do
       end
 
       context 'not affiliated with the campaign' do
-        it 'returns the error messages'
-        it 'returns a status of 401 (Unauthorized)'
+        it 'returns the error messages' do
+          get "/api/campaigns/#{visible_character.campaign_id}/characters/#{visible_character.id}"
+          expect(response.body).to include_json({
+            errors: a_kind_of(Array)
+          })
+        end
+        
+        it 'returns a status of 401 (Unauthorized)' do
+          get "/api/campaigns/#{visible_character.campaign_id}/characters/#{visible_character.id}"
+          expect(response).to have_http_status(:unauthorized)
+        end
       end
     end
     

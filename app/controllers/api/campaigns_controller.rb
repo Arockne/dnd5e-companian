0c8d83a6e9aad264d73e37bb2235551e12b6f002
Daniel Calvin <arockne2003@yahoo.com>
2022-03-20 18:30:26 -0700

    implement update

--- app/controllers/api/campaigns_controller.rb
+++ app/controllers/api/campaigns_controller.rb
@@ -1,5 +1,5 @@
 class Api::CampaignsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def index
     campaigns = Campaign.all
     render json: campaigns, status: :ok
@@ -12,6 +12,13 @@ class Api::CampaignsController < ApplicationController
     else
       render json: { errors: campaign.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def update
+    campaign = current_user.owned_campaigns.find_by_id(params[:id])
+    return render json: { errors: ['Not Authorized'] }, status: :unauthorized if campaign.nil?
+    campaign.update!(campaign_params)
+    render json: campaign, status: :ok
   end
 
   def destroy
@@ -29,4 +36,5 @@ class Api::CampaignsController < ApplicationController
   def campaign_params
     params.require(:campaign).permit(:name, :setting, :password, :password_confirmation)
   end
+
 end

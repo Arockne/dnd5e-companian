b24fd7e0542b4241fd47a02ce10eee7ad4791b5a
Daniel Calvin <arockne2003@yahoo.com>
2022-03-21 15:05:03 -0700

    test attributes to respond to

--- spec/models/character_profile_spec.rb
+++ spec/models/character_profile_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CharacterProfile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'attributes' do
+    [:age, :height, :weight, :eyes, :skin, :hair, :gender, :appearance, :backstory].each do |attr|
+      it { is_expected.to respond_to(attr) }
+    end
+  end
 end

0767a42b9881bf2ecf108ef95a4a923f99916f67
Daniel Calvin <arockne2003@yahoo.com>
2022-03-22 14:32:01 -0700

    test attributes

--- spec/models/campaign_log_spec.rb
+++ spec/models/campaign_log_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CampaignLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'attributes' do
+    it { is_expected.to respond_to(:color) }
+    it { is_expected.to respond_to(:message) }
+    it { is_expected.to respond_to(:campaign_id) }
+  end
 end

dbec9bebeaa07e998b45c32af0c0979a4bb3c198
Daniel Calvin <arockne2003@yahoo.com>
2022-03-22 16:11:39 -0700

    rescue ActiveRecord::RecordInvalid with render_unprocessable_entity

--- app/controllers/api/character_profiles_controller.rb
+++ app/controllers/api/character_profiles_controller.rb
@@ -1,4 +1,5 @@
 class Api::CharacterProfilesController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
   def update
     character_profile = CharacterProfile.find(params[:id])
@@ -14,6 +15,10 @@ class Api::CharacterProfilesController < ApplicationController
 
   def character_profile_params
     params.require(:character_profile).permit(:age, :height, :weight, :eyes, :skin, :hair, :gender, :appearance, :backstory)
+  end
+
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end

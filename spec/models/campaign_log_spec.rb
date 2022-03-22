12c465773949b96ed2cbeddd5eb56920df378167
Daniel Calvin <arockne2003@yahoo.com>
2022-03-22 14:34:00 -0700

    test validations

--- spec/models/campaign_log_spec.rb
+++ spec/models/campaign_log_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe CampaignLog, type: :model do
     it { is_expected.to respond_to(:message) }
     it { is_expected.to respond_to(:campaign_id) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:color) }
+    it { is_expected.to validate_presence_of(:message) }
+  end
 end

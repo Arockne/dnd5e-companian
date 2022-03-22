32a02f9391e84c1813472813939d15f730d6845b
Daniel Calvin <arockne2003@yahoo.com>
2022-03-22 16:06:53 -0700

    implement update action

--- app/controllers/api/character_profiles_controller.rb
+++ app/controllers/api/character_profiles_controller.rb
@@ -1,2 +1,19 @@
 class Api::CharacterProfilesController < ApplicationController
+
+  def update
+    character_profile = CharacterProfile.find(params[:id])
+    if character_profile.character.user == current_user
+      character_profile.update!(character_profile_params)
+      render json: character_profile, status: :ok
+    else
+      render json: { errors: ['Not Authorized'] }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def character_profile_params
+    params.require(:character_profile).permit(:age, :height, :weight, :eyes, :skin, :hair, :gender, :appearance, :backstory)
+  end
+
 end

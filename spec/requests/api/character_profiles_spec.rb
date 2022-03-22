543bb7ce5fd17a4f9ed721624b4ac2368f235b5a
Daniel Calvin <arockne2003@yahoo.com>
2022-03-22 14:59:55 -0700

    add PATCH /update test descriptions

--- spec/requests/api/character_profiles_spec.rb
+++ spec/requests/api/character_profiles_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Api::CharacterProfiles", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "PATCH /update" do
+    context 'when a user is logged in' do
+      context 'as the creator of the character' do
+        it 'returns the updated character'
+        it 'returns a status of 200 (Ok)'
+      end
+      context 'not the creator of the character' do
+        it 'returns error messages'
+        it 'returns a status of 401 (Unauthorized)'
+      end
+    end
+    context 'when a user is not logged in' do
+      it 'returns error messages'
+      it 'returns a 401 (Unauthorized)'
+    end
   end
 end

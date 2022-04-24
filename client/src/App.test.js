1509ad5a8fc9c6327da4f57b7cde1382a562129c
Daniel Calvin <arockne2003@yahoo.com>
2022-04-24 12:21:31 -0700

    update dependencies

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,8 +3,10 @@ import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
+import { createRoot } from 'react-dom/client'
 
 test("renders learn react link", () => {
+  console.log(createRoot)
   const { getByText } = render(
     <Provider store={store}>
       <App />

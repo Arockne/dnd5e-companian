7d1a901097b3d0e4e5bea386ba53bd5c5c74bc1d
Daniel Calvin <arockne2003@yahoo.com>
2022-04-24 12:58:54 -0700

    renew client

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,10 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./App";
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
 
-test("renders learn react link", () => {
-  console.log(createRoot)
+test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />

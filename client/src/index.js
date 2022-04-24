b3dcd526bbbb2c6f6906acf934741abacac8cb59
Daniel Calvin <arockne2003@yahoo.com>
2022-04-24 14:04:16 -0700

    format files

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root');
-const root = createRoot(container)
+const container = document.getElementById("root");
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>

c9416d03635c512428e524a5b7e49973b1b3b6e4
Daniel Calvin <arockne2003@yahoo.com>
2022-04-26 15:32:39 -0700

    implement userSlice

---
+++ client/src/features/user/userSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: {},
+  status: "idle",
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: () => {},
+});
+
+export default userSlice.reducer;

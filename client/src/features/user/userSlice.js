ffdf7e364be1eb4cb948dd8c609f00cf5251d7a6
Daniel Calvin <arockne2003@yahoo.com>
2022-04-28 13:14:21 -0700

    add rejected case for getUser

--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -1,14 +1,23 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { fetchMe } from './userAPI';
 
-export const getUser = createAsyncThunk('user/getUser', async () => {
-  const response = await fetchMe();
-  return response.data;
-});
+export const getUser = createAsyncThunk(
+  'user/getUser',
+  // eslint-disable-next-line no-unused-vars
+  async (_, { rejectWithValue }) => {
+    const response = await fetchMe();
+    const body = await response.json();
+    if (response.ok) {
+      return body;
+    }
+    return rejectWithValue(body);
+  }
+);
 
 const initialState = {
   user: {},
   status: 'idle',
+  errors: null,
 };
 
 export const userSlice = createSlice({
@@ -21,8 +30,12 @@ export const userSlice = createSlice({
         state.status = 'loading';
       })
       .addCase(getUser.fulfilled, (state, action) => {
-        state.status = 'idle';
+        state.status = 'succeeded';
         state.user = action.payload;
+      })
+      .addCase(getUser.rejected, (state, action) => {
+        state.status = 'failed';
+        state.errors = action.payload;
       });
   },
 });

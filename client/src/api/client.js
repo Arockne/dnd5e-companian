6959bcb6a870c1300c48f0e621edf4e2170186c5
Daniel Calvin <arockne2003@yahoo.com>
2022-04-28 17:33:41 -0700

    shorten client function to only fetch data

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,4 +1,4 @@
-export async function client(endpoint, { body, ...customConfig } = {}) {
+export function client(endpoint, { body, ...customConfig } = {}) {
   const headers = { 'content-type': 'application/json' }
   const config = {
     method: body ? 'POST' : 'GET',
@@ -13,20 +13,5 @@ export async function client(endpoint, { body, ...customConfig } = {}) {
     config.body = JSON.stringify(body)
   }
 
-  let data
-  try {
-    const response = await fetch(endpoint, config)
-    data = await response.json()
-    if (response.ok) {
-      return {
-        status: response.status,
-        data,
-        headers: response.headers,
-        url: response.url,
-      }
-    }
-    throw new Error(response.statusText)
-  } catch (err) {
-    return Promise.reject(err.message ? err.message : data)
-  }
+  return fetch(endpoint, config)
 }

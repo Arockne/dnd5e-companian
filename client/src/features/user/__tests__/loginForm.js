4bf0f2004e943ebf522cfe8fa752a0d6cc1629a1
Daniel Calvin <arockne2003@yahoo.com>
2022-04-30 16:25:26 -0700

    implement server setup from msw with handler and test handling server error when logging in

--- client/src/features/user/__tests__/loginForm.js
+++ client/src/features/user/__tests__/loginForm.js
@@ -1,6 +1,20 @@
-import { screen, render } from '@testing-library/react'
+import { screen, render, waitFor } from '../../../tests/test-utils'
 import LoginForm from '../loginForm'
 import userEvent from '@testing-library/user-event'
+import { handlers } from '../../../__mocks__/user'
+import { setupServer } from 'msw/node'
+import { rest } from 'msw'
+
+const server = setupServer(...handlers)
+
+// Enable API mocking before tests.
+beforeAll(() => server.listen())
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers())
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close())
 
 it('renders a form with username and password fields', () => {
   render(<LoginForm />)
@@ -34,4 +48,30 @@ test('button to be initially disabled and enabled when form is filled out', asyn
   await userEvent.clear(password)
   expect(password).toBeEmptyDOMElement()
   expect(submitButton).toBeDisabled()
+})
+
+it('handles server error', async () => {
+  const testData = { username: 'testfailUser', password: 'testFailPassword' }
+  const errorMessage = 'Invalid username or password'
+  server.use(
+    rest.post('/api/login', (req, res, ctx) => {
+      return res(ctx.json({ errors: [errorMessage] }), ctx.status(401))
+    })
+  )
+  render(<LoginForm />)
+  const username = screen.getByLabelText(/username/i)
+  const password = screen.getByLabelText(/password/i)
+  const submitButton = screen.getByRole('button', /submit/i)
+
+  expect(username).toBeEmptyDOMElement()
+  expect(password).toBeEmptyDOMElement()
+
+  await userEvent.type(username, testData.username)
+  await userEvent.type(password, testData.password)
+
+  await userEvent.click(submitButton)
+
+  await waitFor(() => screen.getByText(/invalid/i))
+  expect(screen.getByText(/invalid/i)).toBeInTheDocument()
+  expect(screen.getByText(/invalid/i)).toHaveTextContent(errorMessage)
 })

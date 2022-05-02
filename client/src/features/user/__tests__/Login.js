a8a26da3e06016376740e53ff1deb1288a11ad31
Daniel Calvin <arockne2003@yahoo.com>
2022-05-02 15:04:09 -0700

    test user interaction between login and signing up

---
+++ client/src/features/user/__tests__/Login.js
@@ -0,0 +1,12 @@
+import userEvent from '@testing-library/user-event'
+import { screen, render } from '../../../tests/router-test-utils'
+import Login from './Login'
+
+test('login initially renders login form and I can navigate between loggin and signup', () => {
+  render(<Login />)
+  expect(screen.getByRole('heading')).toHaveTextContent(/welcome/i)
+  userEvent.click(screen.getByText(/create/i))
+  expect(screen.getByText(/sign/i)).toBeInDocument()
+  userEvent.click(screen.getByText(/sign/i))
+  expect(screen.getByText(/create/i)).toBeInDocument()
+})

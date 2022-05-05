65838cf0f2726a91cb67a0cb0d4f546f45b4e87d
Daniel Calvin <arockne2003@yahoo.com>
2022-05-05 15:27:29 -0700

    center login heading

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -7,7 +7,9 @@ import SignUpForm from './SignUpForm'
 function Login() {
   return (
     <>
-      <Title order={1}>Welcome to dnd 5e companion</Title>
+      <Title order={1} align="center">
+        Welcome to dnd 5e companion
+      </Title>
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/signup" element={<SignUpForm />} />

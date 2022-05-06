9bb0019efca24faa2760151d16c6b56a270e2392
Daniel Calvin <arockne2003@yahoo.com>
2022-05-06 10:18:16 -0700

    initial commit

---
+++ client/src/__tests__/App.js
@@ -0,0 +1,21 @@
+import userEvent from '@testing-library/user-event'
+import { screen, render, waitFor } from '../utils/test-utils'
+import App from '../App'
+
+test('user is able to login', async () => {
+  const testUser = { username: 'test', password: 'testpassword' }
+
+  render(<App />)
+
+  const username = screen.getByRole('textbox', { name: /username/i })
+  const password = screen.getByLabelText(/password \*/i)
+  const submit = screen.getByRole('button', { name: /sign in/i })
+
+  await userEvent.type(username, testUser.username)
+  expect(username).toHaveValue(testUser.username)
+
+  await userEvent.type(password, testUser.password)
+  expect(password).toHaveValue(testUser.password)
+
+  await userEvent.click(submit)
+})

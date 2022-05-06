df35d50d27a22735d734f2d891a4f1927530b249
Daniel Calvin <arockne2003@yahoo.com>
2022-05-06 08:07:26 -0700

    refactor to remove useForm hook

--- client/src/login/LoginForm.js
+++ client/src/login/LoginForm.js
@@ -9,61 +9,58 @@ import {
   ThemeIcon,
   Group,
 } from '@mantine/core'
-import { useForm } from '@mantine/form'
 import { AlertCircle } from 'tabler-icons-react'
 import { useDispatch, useSelector } from 'react-redux'
 import { loginUser, reset } from '../user/state/userActions'
 import { Link } from 'react-router-dom'
 
 function LoginForm() {
-  const form = useForm({
-    initialValues: {
-      username: '',
-      password: '',
-    },
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
   })
 
   const dispatch = useDispatch()
   const { status, errors } = useSelector((state) => state.user)
 
-  useEffect(() => {
-    if (status === 'failed') {
-      form.setErrors(errors)
-    }
-    if (status === 'loading') {
-      form.clearErrors()
-    }
-    return () => {
-      if (status !== 'succeeded') {
-        dispatch(reset())
-      }
-    }
-  }, [status])
-
-  const enabled = Object.entries(form.values).every(
+  const enabled = Object.entries(formData).every(
     ([_, value]) => value.length > 0
   )
 
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch(loginUser(formData))
+    if (status === 'successful') {
+      setFormData({
+        username: '',
+        password: '',
+      })
+    }
+  }
+
   return (
     <Box sx={{ maxWidth: 400 }} mx="auto">
-      <form
-        style={{ position: 'relative' }}
-        onSubmit={form.onSubmit((values) => dispatch(loginUser(values)))}
-      >
+      <form style={{ position: 'relative' }} onSubmit={handleSubmit}>
         <LoadingOverlay visible={status === 'loading'} />
         <TextInput
           required
           autoComplete="username"
           label="Username"
           placeholder="Username"
-          {...form.getInputProps('username')}
+          name="username"
+          onChange={handleChange}
         />
         <PasswordInput
           required
           autoComplete="current-password"
           label="Password"
           placeholder="Password"
-          {...form.getInputProps('password')}
+          name="password"
+          onChange={handleChange}
         />
         <List
           withPadding
@@ -75,8 +72,8 @@ function LoginForm() {
             </ThemeIcon>
           }
         >
-          {form.errors && Array.isArray(form.errors)
-            ? form.errors.map((error) => (
+          {errors
+            ? errors.map((error) => (
                 <List.Item key={error} sx={{ color: '#EE4B2B' }}>
                   {error}
                 </List.Item>
@@ -88,7 +85,9 @@ function LoginForm() {
             Sign in
           </Button>
         </Group>
-        <Link to="/signup">Create new account</Link>
+        <Link to="/signup" onClick={() => dispatch(reset())}>
+          Create new account
+        </Link>
       </form>
     </Box>
   )

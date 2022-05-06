456816d96a63ab8d8b69d629bbb86eda63199521
Daniel Calvin <arockne2003@yahoo.com>
2022-05-06 12:35:43 -0700

    fix mocks to have correct response

--- client/src/__mocks__/user.js
+++ client/src/__mocks__/user.js
@@ -2,10 +2,10 @@ import { rest } from 'msw'
 
 export const handlers = [
   rest.post('/api/login', (req, res, ctx) => {
-    const { username } = req
-    return res(ctx.res({ username }), ctx.status(201))
+    const { username } = req.body
+    return res(ctx.json({ username }), ctx.status(201))
   }),
   rest.post('/api/signup', (req, res, ctx) => {
-    return res(ctx.json(201))
+    return res(ctx.status(201))
   }),
 ]

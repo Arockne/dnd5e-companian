0ddaf93c47808d435abdf0359b76b7fcc69e8dec
Daniel Calvin <arockne2003@yahoo.com>
2022-05-09 15:45:20 -0700

    add dropdown menu

--- client/src/features/navigation/Header.js
+++ client/src/features/navigation/Header.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react'
 import {
   createStyles,
-  Header,
   Container,
+  Avatar,
+  UnstyledButton,
   Group,
+  Text,
+  Menu,
+  Divider,
+  Tabs,
   Burger,
-  Title,
   Anchor,
+  Header,
+  Title,
 } from '@mantine/core'
 import { useBooleanToggle } from '@mantine/hooks'
 import { Link } from 'react-router-dom'
+import {
+  Logout,
+  Heart,
+  Star,
+  Message,
+  Settings,
+  PlayerPause,
+  Trash,
+  SwitchHorizontal,
+  ChevronDown,
+} from 'tabler-icons-react'
 
 const useStyles = createStyles((theme) => {
   return {
@@ -65,13 +82,39 @@ const useStyles = createStyles((theme) => {
           ],
       },
     },
+
+    userMenu: {
+      [theme.fn.smallerThan('xs')]: {
+        display: 'none',
+      },
+    },
+
+    user: {
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      transition: 'background-color 100ms ease',
+
+      '&:hover': {
+        backgroundColor:
+          theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+      },
+    },
   }
 })
 
+const user = {
+  name: 'Jane Spoonfighter',
+  email: 'janspoon@fighter.dev',
+  image:
+    'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80',
+}
+
 function MainHeader({ links }) {
   const [opened, toggleOpened] = useBooleanToggle(false)
+  const [userMenuOpened, setUserMenuOpened] = useState(false)
   const [active, setActive] = useState('')
-  const { classes, cx } = useStyles()
+  const { classes, theme, cx } = useStyles()
 
   const items = links.map((link) => (
     <Anchor
@@ -95,6 +138,68 @@ function MainHeader({ links }) {
         <Title order={3}>D&D 5e Companion</Title>
         <Group spacing={5} className={classes.links}>
           {items}
+        </Group>
+
+        <Group position="apart">
+          <Menu
+            size={260}
+            placement="end"
+            transition="pop-top-right"
+            className={classes.userMenu}
+            onClose={() => setUserMenuOpened(false)}
+            onOpen={() => setUserMenuOpened(true)}
+            control={
+              <UnstyledButton
+                className={cx(classes.user, {
+                  [classes.userActive]: userMenuOpened,
+                })}
+              >
+                <Group spacing={7}>
+                  <Avatar
+                    src={user.image}
+                    alt={user.name}
+                    radius="xl"
+                    size={20}
+                  />
+                  <Text weight={500} size="sm" sx={{ lineHeight: 1 }} mr={3}>
+                    {user.name}
+                  </Text>
+                  <ChevronDown size={12} />
+                </Group>
+              </UnstyledButton>
+            }
+          >
+            <Menu.Item icon={<Heart size={14} color={theme.colors.red[6]} />}>
+              Liked posts
+            </Menu.Item>
+            <Menu.Item icon={<Star size={14} color={theme.colors.yellow[6]} />}>
+              Saved posts
+            </Menu.Item>
+            <Menu.Item
+              icon={<Message size={14} color={theme.colors.blue[6]} />}
+            >
+              Your comments
+            </Menu.Item>
+
+            <Menu.Label>Settings</Menu.Label>
+            <Menu.Item icon={<Settings size={14} />}>
+              Account settings
+            </Menu.Item>
+            <Menu.Item icon={<SwitchHorizontal size={14} />}>
+              Change account
+            </Menu.Item>
+            <Menu.Item icon={<Logout size={14} />}>Logout</Menu.Item>
+
+            <Divider />
+
+            <Menu.Label>Danger zone</Menu.Label>
+            <Menu.Item icon={<PlayerPause size={14} />}>
+              Pause subscription
+            </Menu.Item>
+            <Menu.Item color="red" icon={<Trash size={14} />}>
+              Delete account
+            </Menu.Item>
+          </Menu>
         </Group>
 
         <Burger

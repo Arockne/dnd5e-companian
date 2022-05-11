2e8c2b7bb1f4a4ec915b6876c084b132c58b8b6b
Daniel Calvin <arockne2003@yahoo.com>
2022-05-10 19:38:24 -0700

    change name from userReducer to userSlice

---
+++ client/src/features/user/state/userSlice.js
@@ -0,0 +1,113 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../../api/client'
+
+export const getCurrentUser = createAsyncThunk(
+  'user/getCurrentUser',
+  async (_, { rejectWithValue }) => {
+    const response = await client.get('/api/me')
+    const body = await response.json()
+    if (response.ok) {
+      return body
+    }
+    return rejectWithValue(body)
+  }
+)
+
+export const createUser = createAsyncThunk(
+  'user/createUser',
+  async (user, { rejectWithValue }) => {
+    const response = await client.post('/api/signup', { user: user })
+    const body = await response.json()
+    if (response.ok) {
+      return body
+    }
+    return rejectWithValue(body)
+  }
+)
+
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (user, { rejectWithValue }) => {
+    const response = await client.post('/api/login', user)
+    const body = await response.json()
+    if (response.ok) {
+      return body
+    }
+    return rejectWithValue(body)
+  }
+)
+
+export const logoutUser = createAsyncThunk(
+  'user/logoutUser',
+  async (_, { rejectWithValue }) => {
+    const response = await client.delete(`/api/logout`)
+    const body = await response.json()
+    if (response.ok) {
+      return {}
+    }
+    return rejectWithValue(body)
+  }
+)
+
+const initialState = {
+  user: null,
+  status: 'idle',
+  errors: null,
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    reset: () => ({ ...initialState, authenticated: true }),
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCurrentUser.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(getCurrentUser.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.user = action.payload
+      })
+      .addCase(getCurrentUser.rejected, (state, action) => {
+        state.status = 'failed'
+      })
+      .addCase(createUser.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(createUser.fulfilled, (state) => {
+        state.status = 'succeeded'
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.status = 'failed'
+        state.errors = action.payload.errors
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.user = action.payload
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.status = 'failed'
+        state.errors = action.payload.errors
+      })
+      .addCase(logoutUser.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(logoutUser.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.user = action.payload
+      })
+      .addCase(logoutUser.rejected, (state, action) => {
+        state.status = 'failed'
+        state.errors = action.payload.errors
+      })
+  },
+})
+
+export const { reset } = userSlice.actions
+
+export default userSlice.reducer

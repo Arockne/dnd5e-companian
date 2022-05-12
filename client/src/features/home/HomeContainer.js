8eccd75a2f8d338e343f1d28f2f240e23ca8314c
Daniel Calvin <arockne2003@yahoo.com>
2022-05-12 13:13:30 -0700

    move loading component to render on page load

--- client/src/features/home/HomeContainer.js
+++ client/src/features/home/HomeContainer.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import { Group } from '@mantine/core'
+import React, { useEffect, useState } from 'react'
+import { Loader } from 'tabler-icons-react'
 import { MAINHEADERLINKS } from '../../constants'
 import MainHeader from '../navigation/Header'
 
 function HomeContainer() {
-  return (
+  const [pageLoad, setPageLoad] = useState(false)
+
+  useEffect(() => {
+    setPageLoad(true)
+  }, [])
+
+  return pageLoad ? (
     <div>
       <MainHeader links={MAINHEADERLINKS} />
       <p>{`Welcome`}</p>
     </div>
+  ) : (
+    <Group sx={{ justifyContent: 'center', height: '100vh' }}>
+      <Loader size="100" variant="bars" />
+    </Group>
   )
 }
 
--- client/src/features/login/Login.js
+++ client/src/features/login/Login.js
@@ -1,11 +1,17 @@
-import { Title } from '@mantine/core'
-import React from 'react'
+import { Group, Loader, Title } from '@mantine/core'
+import React, { useEffect, useState } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginForm from './LoginForm'
 import SignUpForm from './SignUpForm'
 
 function Login() {
-  return (
+  const [pageLoad, setPageLoad] = useState(false)
+
+  useEffect(() => {
+    setPageLoad(true)
+  }, [])
+
+  return pageLoad ? (
     <>
       <Title order={1} align="center">
         Welcome to D&D 5e Companion
@@ -16,6 +22,10 @@ function Login() {
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
+  ) : (
+    <Group sx={{ justifyContent: 'center', height: '100vh' }}>
+      <Loader size="100" variant="bars" />
+    </Group>
   )
 }
 

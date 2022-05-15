57ccb53ba674b050a1f74302207895cc9ef4ea22
Daniel Calvin <arockne2003@yahoo.com>
2022-05-15 09:12:41 -0700

    add campaignReducer to store

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import campaignReducer from './features/campaign/campaignSlice'
 import userReducer from './features/user/state/userSlice'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    campaign: campaignReducer,
   },
 })

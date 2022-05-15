99782c6cd99c989ee4046e1bf5a6c6a0ee83fe83
Daniel Calvin <arockne2003@yahoo.com>
2022-05-15 09:22:15 -0700

    implement getCampaigns

--- client/src/features/campaign/campaignSlice.js
+++ client/src/features/campaign/campaignSlice.js
@@ -1,5 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
+
+export const getCampaigns = createAsyncThunk(
+  'campaign/getCampaigns',
+  async (_, { rejectWithValue }) => {
+    const response = await client.get('/api/campaigns')
+    const body = await response.json()
+    if (response.ok) {
+      return body
+    }
+    return rejectWithValue(body)
+  }
+)
 
 const initialState = {
   campaign: null,

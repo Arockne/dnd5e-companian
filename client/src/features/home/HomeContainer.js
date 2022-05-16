dbececd0925441c795c1d953d3491c34189acf04
Daniel Calvin <arockne2003@yahoo.com>
2022-05-16 15:46:26 -0700

    add /campaigns/search route

--- client/src/features/home/HomeContainer.js
+++ client/src/features/home/HomeContainer.js
@@ -2,6 +2,7 @@ import { Group } from '@mantine/core'
 import React, { useEffect, useState } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Loader } from 'tabler-icons-react'
+import CampaignContainer from '../campaign/campaignContainer'
 import MainHeader from '../navigation/MainHeader'
 
 function HomeContainer() {
@@ -15,7 +16,7 @@ function HomeContainer() {
     <Routes>
       <Route path="/" element={<MainHeader />}>
         <Route path="campaigns">
-          <Route path="search" />
+          <Route path="search" element={<CampaignContainer />} />
         </Route>
       </Route>
       <Route path="*" element={<Navigate to="/" />} />

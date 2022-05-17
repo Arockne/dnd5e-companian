7af266ca03cfdbbd44a327cb191102b80f6d0381
Daniel Calvin <arockne2003@yahoo.com>
2022-05-17 14:59:40 -0700

    render each campaign

--- client/src/features/campaign/campaignContainer.js
+++ client/src/features/campaign/campaignContainer.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCampaigns } from './campaignSlice'
+import { Stack, TextInput, Container } from '@mantine/core'
+import CampaignCard from './CampaignCard'
 
 function CampaignContainer() {
-  return <div></div>
+  const dispatch = useDispatch()
+  const { campaigns, status } = useSelector((state) => state.campaign)
+
+  useEffect(() => {
+    dispatch(getCampaigns())
+  }, [])
+
+  return status === 'succeeded' ? (
+    <Stack
+      align="flex-start"
+      justify="stretch"
+      sx={(theme) => ({
+        backgroundColor:
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[8]
+            : theme.colors.gray[0],
+      })}
+    >
+      <Container>
+        <TextInput
+          label="Search for a Campaign:"
+          placeholder="Campaign Search"
+          size="xs"
+        />
+      </Container>
+      <Container>
+        {campaigns.map((campaign) => (
+          <CampaignCard key={campaign.id} campaign={campaign} />
+        ))}
+      </Container>
+    </Stack>
+  ) : null
 }
 
 export default CampaignContainer

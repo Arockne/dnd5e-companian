04f90f8478cc944df3ff00a32d127e03411815b7
Daniel Calvin <arockne2003@yahoo.com>
2022-05-19 15:03:15 -0700

    implement joinCampaign

--- client/src/features/campaign-user/campaignUserSlice.js
+++ client/src/features/campaign-user/campaignUserSlice.js
@@ -1,6 +1,20 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
 
+const joinCampaign = createAsyncThunk(
+  'campaignUser/joinCampaign',
+  async (campaign, { rejectWithValue }) => {
+    const response = await client.post(`/api/campaigns/${campaign.id}`, {
+      campaign,
+    })
+    const data = await response
+    if (response.ok) {
+      return
+    }
+    rejectWithValue(data)
+  }
+)
+
 const initialState = {
   status: 'idle',
   errors: null,

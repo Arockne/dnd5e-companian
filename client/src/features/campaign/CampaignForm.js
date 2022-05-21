b1660260fb22916ec7c9efbfd8a0ced1b8e1205e
Daniel Calvin <arockne2003@yahoo.com>
2022-05-21 08:54:11 -0700

    implement controlled form

--- client/src/features/campaign/CampaignForm.js
+++ client/src/features/campaign/CampaignForm.js
@@ -6,18 +6,46 @@ import {
   Textarea,
   TextInput,
 } from '@mantine/core'
-import React from 'react'
+import React, { useState } from 'react'
 
 function CampaignForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    image_url: '',
+    setting: '',
+    password: '',
+  })
+
+  function handleChange(e) {
+    const { name, value } = e.target
+    const updatedFormData = { ...formData, [name]: value }
+    setFormData(updatedFormData)
+  }
+  console.log(formData)
   return (
     <Box sx={{ maxWidth: 400 }} mx="auto">
       <form style={{ position: 'relative' }}>
-        <TextInput required label="Name" placeholder="Name" name="name" />
-        <TextInput label="Image" name="image_url" />
+        <TextInput
+          required
+          label="Name"
+          placeholder="Name"
+          name="name"
+          onChange={handleChange}
+          value={formData.name}
+        />
+        <TextInput
+          label="Image"
+          name="image_url"
+          onChange={handleChange}
+          value={formData.image_url}
+        />
         <Textarea
           required
           label="Setting"
           placeholder="In a place unbeknownst to man..."
+          name="setting"
+          onChange={handleChange}
+          value={formData.setting}
         />
         <PasswordInput
           required
@@ -25,6 +53,8 @@ function CampaignForm() {
           label="Password"
           placeholder="Password"
           name="password"
+          onChange={handleChange}
+          value={formData.password}
         />
         <Group position="right" mt="md">
           <Button type="submit">Create Adventure!</Button>

56637c7b116d9a371ca05cc544d8160083791f5d
Daniel Calvin <arockne2003@yahoo.com>
2022-05-23 13:05:35 -0700

    implement getting users campaigns

--- client/src/features/campaign/CampaignsContainer.js
+++ client/src/features/campaign/CampaignsContainer.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCurrentCampaigns } from './campaignSlice'
+import CampaignsTable from './CampaignTable'
 
 function CampaignsContainer() {
+  const dispatch = useDispatch()
+  const { owned_campaigns, campaigns } = useSelector(
+    (state) => state.campaign.campaigns
+  )
+  useEffect(() => {
+    dispatch(getCurrentCampaigns())
+  }, [])
+
   return <div></div>
 }
 

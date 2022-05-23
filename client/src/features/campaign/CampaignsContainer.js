e8b41eea4c1a57fe60f1ba99c9f03285e5b3da44
Daniel Calvin <arockne2003@yahoo.com>
2022-05-23 14:03:14 -0700

    implement structure

--- client/src/features/campaign/CampaignsContainer.js
+++ client/src/features/campaign/CampaignsContainer.js
@@ -1,3 +1,4 @@
+import { Container, Group, Loader, Stack, Title } from '@mantine/core'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCurrentCampaigns } from './campaignSlice'
@@ -5,14 +6,34 @@ import CampaignsTable from './CampaignTable'
 
 function CampaignsContainer() {
   const dispatch = useDispatch()
-  const { owned_campaigns, campaigns } = useSelector(
-    (state) => state.campaign.campaigns
-  )
+  const {
+    campaigns: { owned_campaigns, campaigns },
+  } = useSelector((state) => state.campaign)
+
   useEffect(() => {
     dispatch(getCurrentCampaigns())
   }, [])
 
-  return <div></div>
+  return campaigns ? (
+    <Stack>
+      <Container>
+        <Title align="center" order={2}>
+          Created Campaigns
+        </Title>
+        <CampaignsTable campaigns={owned_campaigns} />
+      </Container>
+      <Container>
+        <Title align="center" order={2}>
+          Campaigns a Member of
+        </Title>
+        <CampaignsTable campaigns={campaigns} />
+      </Container>
+    </Stack>
+  ) : (
+    <Group position="center">
+      <Loader size="xl" />
+    </Group>
+  )
 }
 
 export default CampaignsContainer

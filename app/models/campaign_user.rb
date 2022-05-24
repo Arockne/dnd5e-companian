e2ecf745b07df3e3f59b06bb5a4e7301243b00f6
Daniel Calvin <arockne2003@yahoo.com>
2022-05-24 10:54:45 -0700

    add custom message for when user attempts to join a campaign that they are part of

--- app/models/campaign_user.rb
+++ app/models/campaign_user.rb
@@ -2,5 +2,10 @@ class CampaignUser < ApplicationRecord
   belongs_to :user
   belongs_to :campaign
 
-  validates :user_id, uniqueness: { scope: :campaign_id }
+  validates :user_id, uniqueness: { 
+    scope: :campaign_id, 
+    message: -> (object, data) do
+      "is already a player of #{object.campaign.name}"
+    end 
+  }
 end

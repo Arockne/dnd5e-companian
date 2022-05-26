6d70d9d3dae24de7ebe0fe1baf461093b9a7fa6e
Daniel Calvin <arockne2003@yahoo.com>
2022-05-26 15:12:06 -0700

    use campaign id to navigate campaign

--- client/src/features/campaign/CampaignHeader.js
+++ client/src/features/campaign/CampaignHeader.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import { Anchor, Grid, Group, Stack, Tabs } from '@mantine/core'
+import React, { useEffect } from 'react'
+import { Anchor, Grid, Group, Stack, Tabs, Title } from '@mantine/core'
 import { MessageCircle, Photo, Settings } from 'tabler-icons-react'
-import { NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet, useParams } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { getCampaign } from './campaignSlice'
 
 export function CampaignHeader() {
+  const { id } = useParams()
+
   return (
     <Grid grow>
       <Grid.Col span={1}>
         <Stack>
-          <NavLink to="/campaigns/1">Overview</NavLink>
-          <NavLink to="/campaigns/1">Players</NavLink>
-          <NavLink to="/campaigns/1">Messages</NavLink>
-          <NavLink to="/campaigns/1">Logs</NavLink>
-          <NavLink to="/campaigns/1">Settings</NavLink>
+          <NavLink to={`/campaigns/${id}`}>Overview</NavLink>
+          <NavLink to={`/campaigns/${id}`}>Players</NavLink>
+          <NavLink to={`/campaigns/${id}`}>Messages</NavLink>
+          <NavLink to={`/campaigns/${id}`}>Logs</NavLink>
+          <NavLink to={`/campaigns/${id}`}>Settings</NavLink>
         </Stack>
       </Grid.Col>
       <Grid.Col span={9}>
+        <Title order={2}>This is the Campaign Title</Title>
         <Outlet />
       </Grid.Col>
     </Grid>

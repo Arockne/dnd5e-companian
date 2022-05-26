94a91f95e9b4d34f5cee7d591b10e8104a98794c
Daniel Calvin <arockne2003@yahoo.com>
2022-05-26 15:24:02 -0700

    get the campaign based on the id

--- client/src/features/campaign/CampaignHeader.js
+++ client/src/features/campaign/CampaignHeader.js
@@ -2,11 +2,18 @@ import React, { useEffect } from 'react'
 import { Anchor, Grid, Group, Stack, Tabs, Title } from '@mantine/core'
 import { MessageCircle, Photo, Settings } from 'tabler-icons-react'
 import { NavLink, Outlet, useParams } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getCampaign } from './campaignSlice'
 
 export function CampaignHeader() {
   const { id } = useParams()
+  const dispatch = useDispatch()
+
+  const { campaign } = useSelector((state) => state.campaign)
+
+  useEffect(() => {
+    dispatch(getCampaign(id))
+  }, [id])
 
   return (
     <Grid grow>
@@ -20,7 +27,7 @@ export function CampaignHeader() {
         </Stack>
       </Grid.Col>
       <Grid.Col span={9}>
-        <Title order={2}>This is the Campaign Title</Title>
+        <Title order={2}>{campaign?.name}</Title>
         <Outlet />
       </Grid.Col>
     </Grid>

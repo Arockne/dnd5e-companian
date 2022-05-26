39dba3cbfb5943e31c7a4b0865a53ba1c6e95b16
Daniel Calvin <arockne2003@yahoo.com>
2022-05-26 14:51:47 -0700

    add structure

--- client/src/features/campaign/CampaignHeader.js
+++ client/src/features/campaign/CampaignHeader.js
@@ -1,23 +1,24 @@
 import React from 'react'
-import { Tabs } from '@mantine/core'
+import { Anchor, Grid, Group, Stack, Tabs } from '@mantine/core'
 import { MessageCircle, Photo, Settings } from 'tabler-icons-react'
+import { NavLink, Outlet } from 'react-router-dom'
 
 export function CampaignHeader() {
   return (
-    <Tabs variant="outline" orientation="vertical">
-      <Tabs.Tab label="Overview" icon={<Photo size={14} />}>
-        Overview tab content
-      </Tabs.Tab>
-      <Tabs.Tab label="Players" icon={<Photo size={14} />}>
-        Players tab content
-      </Tabs.Tab>
-      <Tabs.Tab label="Messages" icon={<MessageCircle size={14} />}>
-        Messages tab content
-      </Tabs.Tab>
-      <Tabs.Tab label="Logs" icon={<Settings size={14} />}>
-        Logs tab content
-      </Tabs.Tab>
-    </Tabs>
+    <Grid grow>
+      <Grid.Col span={1}>
+        <Stack>
+          <NavLink to="/campaigns/1">Overview</NavLink>
+          <NavLink to="/campaigns/1">Players</NavLink>
+          <NavLink to="/campaigns/1">Messages</NavLink>
+          <NavLink to="/campaigns/1">Logs</NavLink>
+          <NavLink to="/campaigns/1">Settings</NavLink>
+        </Stack>
+      </Grid.Col>
+      <Grid.Col span={9}>
+        <Outlet />
+      </Grid.Col>
+    </Grid>
   )
 }
 

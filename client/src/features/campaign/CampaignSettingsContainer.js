b30b334b70de1cb411306624db4ed7173542b740
Daniel Calvin <arockne2003@yahoo.com>
2022-05-29 09:51:01 -0700

    add default navigation to hoome when no campaign

--- client/src/features/campaign/CampaignSettingsContainer.js
+++ client/src/features/campaign/CampaignSettingsContainer.js
@@ -1,11 +1,19 @@
 import { Group } from '@mantine/core'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import CampaignDeleteContainer from './CampaignDeleteContainer'
 import CampaignUpdateForm from './CampaignUpdateForm'
 
 function CampaignSettingsContainer() {
   const { campaign, status } = useSelector((state) => state.campaign)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!campaign) {
+      navigate('/')
+    }
+  }, [campaign])
 
   return (
     <Group direction="column">

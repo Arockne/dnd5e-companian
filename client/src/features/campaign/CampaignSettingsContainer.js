5695eab193ef5f691b1b0c679d0093a480086d80
Daniel Calvin <arockne2003@yahoo.com>
2022-05-29 13:50:18 -0700

    remove redirect if there is no campaign

--- client/src/features/campaign/CampaignSettingsContainer.js
+++ client/src/features/campaign/CampaignSettingsContainer.js
@@ -1,19 +1,11 @@
 import { Group } from '@mantine/core'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
 import CampaignDeleteContainer from './CampaignDeleteContainer'
 import CampaignUpdateContainer from './CampaignUpdateContainer'
 
 function CampaignSettingsContainer() {
   const { campaign, status } = useSelector((state) => state.campaign)
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    if (!campaign) {
-      navigate('/')
-    }
-  }, [campaign])
 
   return (
     <Group direction="column">

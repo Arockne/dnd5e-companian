d534a0330d2ea668ca16d0c9be08ee6ea2511b02
Daniel Calvin <arockne2003@yahoo.com>
2022-05-29 18:12:12 -0700

    implement controlled form

--- client/src/features/campaign/settings/CampaignUpdatePasswordForm.js
+++ client/src/features/campaign/settings/CampaignUpdatePasswordForm.js
@@ -1,12 +1,41 @@
 import { Button, Group, PasswordInput } from '@mantine/core'
-import React from 'react'
+import React, { useState } from 'react'
 
 function CampaignUpdatePasswordForm() {
+  const [formData, setFormData] = useState({
+    old_password: '',
+    new_password: '',
+    new_password_confirmation: '',
+  })
+
+  function handleChange(e) {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
   return (
     <form>
-      <PasswordInput label="Old password" />
-      <PasswordInput label="New password" />
-      <PasswordInput label="New password confirmation" />
+      <PasswordInput
+        required
+        label="Old password"
+        name="old_password"
+        value={formData.old_password}
+        onChange={handleChange}
+      />
+      <PasswordInput
+        required
+        label="New password"
+        name="new_password"
+        value={formData.new_password}
+        onChange={handleChange}
+      />
+      <PasswordInput
+        required
+        label="New password confirmation"
+        name="new_password_confirmation"
+        value={formData.new_password_confirmation}
+        onChange={handleChange}
+      />
       <Group position="left" mt="md">
         <Button type="submit">Update password</Button>
       </Group>

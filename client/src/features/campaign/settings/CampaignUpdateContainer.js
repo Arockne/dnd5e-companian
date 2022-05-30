e52ac8771a71a554663683eaf0ce9ccd71274096
Daniel Calvin <arockne2003@yahoo.com>
2022-05-29 17:38:43 -0700

    add titles to forms

--- client/src/features/campaign/settings/CampaignUpdateContainer.js
+++ client/src/features/campaign/settings/CampaignUpdateContainer.js
@@ -1,10 +1,17 @@
+import { Divider, Title } from '@mantine/core'
 import React from 'react'
 import CampaignUpdateForm from './CampaignUpdateForm'
+import CampaignUpdatePasswordForm from './CampaignUpdatePasswordForm'
 
 function CampaignUpdateContainer({ campaign, status }) {
   return (
     <>
+      <Title order={3}>Update Campaign</Title>
+      <Divider />
       <CampaignUpdateForm campaign={campaign} status={status} />
+      <Title order={3}>Update Password</Title>
+      <Divider />
+      <CampaignUpdatePasswordForm />
     </>
   )
 }

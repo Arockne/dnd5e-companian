67a2e07ff1b1de85174b0db7786c20fa60e3cb70
Daniel Calvin <arockne2003@yahoo.com>
2022-05-30 13:44:46 -0700

    implement setting status and errors from response

--- client/src/features/campaign/settings/CampaignUpdatePasswordForm.js
+++ client/src/features/campaign/settings/CampaignUpdatePasswordForm.js
@@ -1,20 +1,43 @@
 import { Button, Group, PasswordInput } from '@mantine/core'
 import React, { useState } from 'react'
+import { client } from '../../../api/client'
 
-function CampaignUpdatePasswordForm() {
+function CampaignUpdatePasswordForm({ campaign }) {
   const [formData, setFormData] = useState({
     old_password: '',
     new_password: '',
     new_password_confirmation: '',
   })
+  const [errors, setErrors] = useState([])
+  const [status, setStatus] = useState('idle')
 
   function handleChange(e) {
     const { name, value } = e.target
     setFormData({ ...formData, [name]: value })
   }
 
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const { old_password, new_password } = formData
+
+    const response = await client.patch(
+      `/api/campaigns/${campaign.id}/password`,
+      {
+        campaign: { old_password, new_password },
+      }
+    )
+
+    if (response.ok) {
+      setStatus('succeeded')
+    } else {
+      const body = await response.json()
+      setErrors(body.errors)
+      setStatus('failed')
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <PasswordInput
         required
         label="Old password"

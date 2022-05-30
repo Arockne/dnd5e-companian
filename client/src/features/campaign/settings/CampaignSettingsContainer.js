398573bed316d1bddbe489604a111cd3e8e3e015
Daniel Calvin <arockne2003@yahoo.com>
2022-05-30 10:03:21 -0700

    replace Group component with Container

--- client/src/features/campaign/settings/CampaignSettingsContainer.js
+++ client/src/features/campaign/settings/CampaignSettingsContainer.js
@@ -1,4 +1,4 @@
-import { Group } from '@mantine/core'
+import { Container } from '@mantine/core'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import CampaignDeleteContainer from './CampaignDeleteContainer'
@@ -8,14 +8,14 @@ function CampaignSettingsContainer() {
   const { campaign, status, errors } = useSelector((state) => state.campaign)
 
   return (
-    <Group direction="column">
+    <Container>
       <CampaignUpdateContainer
         campaign={campaign}
         status={status}
         errors={errors}
       />
       <CampaignDeleteContainer campaign={campaign} status={status} />
-    </Group>
+    </Container>
   )
 }
 

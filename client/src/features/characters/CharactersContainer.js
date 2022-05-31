a980965dbe31ac5a4b05e57295ac134ad3d37cfe
Daniel Calvin <arockne2003@yahoo.com>
2022-05-31 12:19:33 -0700

    implement CharactersContainer

---
+++ client/src/features/characters/CharactersContainer.js
@@ -0,0 +1,12 @@
+import { Stack, Title } from '@mantine/core'
+import React from 'react'
+
+function CharactersContainer() {
+  return (
+    <Stack align="center">
+      <Title order={2}>Characters</Title>
+    </Stack>
+  )
+}
+
+export default CharactersContainer

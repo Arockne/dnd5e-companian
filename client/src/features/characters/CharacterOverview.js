fdba38ded9bbb19a917bc8fc801fa23407ea1707
Daniel Calvin <arockne2003@yahoo.com>
2022-06-03 15:51:41 -0700

    add structure to page

---
+++ client/src/features/characters/CharacterOverview.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Skeleton, Container, Group, Image, Grid, Title } from '@mantine/core'
+
+const child = <Skeleton height={140} radius="md" animate={false} />
+
+export function CharacterOverview() {
+  return (
+    <Container my="md">
+      <Grid>
+        <Grid.Col xs={4}>
+          <Grid>
+            <Grid.Col>
+              <Group position="center">
+                <Image
+                  alt="Character image"
+                  withPlaceholder={true}
+                  radius="lg"
+                  height={140}
+                />
+              </Group>
+            </Grid.Col>
+            <Grid.Col>
+              <Group position="center">
+                <Skeleton height={250} radius="md" animate={false} />
+              </Group>
+            </Grid.Col>
+            <Grid.Col>
+              <Group position="center">
+                <Skeleton height={100} radius="md" animate={false} />
+              </Group>
+            </Grid.Col>
+          </Grid>
+        </Grid.Col>
+        <Grid.Col xs={6}>
+          <Grid>
+            <Grid.Col>
+              <Group position="center">
+                <Skeleton height={70} radius="md" animate={false} />
+              </Group>
+            </Grid.Col>
+            <Grid.Col>
+              <Group>
+                <Skeleton height={435} radius="md" animate={false} />
+              </Group>
+            </Grid.Col>
+          </Grid>
+        </Grid.Col>
+      </Grid>
+    </Container>
+  )
+}
+
+export default CharacterOverview

f6a24f715999384b1211ff7fcfc1f82d26d7e96d
Daniel Calvin <arockne2003@yahoo.com>
2022-06-03 17:17:25 -0700

    add characters route

--- client/src/features/campaign/Campaign.js
+++ client/src/features/campaign/Campaign.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Route, Routes, useParams } from 'react-router-dom'
+import Character from '../characters/Character'
 import CampaignHeader from './CampaignHeader'
 import CampaignOverview from './CampaignOverview'
 import { getCampaign } from './campaignSlice'
 import CampaignSettingsContainer from './settings/CampaignSettingsContainer'
 
 function Campaign() {
-  const { id } = useParams()
+  const { campaign_id } = useParams()
   const dispatch = useDispatch()
 
   const { campaign } = useSelector((state) => state.campaign)
@@ -16,10 +17,10 @@ function Campaign() {
   const owner = campaign?.owner?.id === user?.id
 
   useEffect(() => {
-    if (campaign?.id !== id) {
-      dispatch(getCampaign(id))
+    if (campaign?.id !== campaign_id) {
+      dispatch(getCampaign(campaign_id))
     }
-  }, [id])
+  }, [campaign_id])
 
   return (
     <Routes>
@@ -32,6 +33,7 @@ function Campaign() {
           <Route path="settings" element={<CampaignSettingsContainer />} />
         ) : null}
       </Route>
+      <Route path="/characters/:character_id/*" element={<Character />} />
     </Routes>
   )
 }

d73c6d2b19ac6fe3be61403f0a01b293e6b81ea1
Daniel Calvin <arockne2003@yahoo.com>
2022-06-03 20:39:51 -0700

    add has one character profile relationship

--- app/serializers/character_show_serializer.rb
+++ app/serializers/character_show_serializer.rb
@@ -1,3 +1,5 @@
 class CharacterShowSerializer < ActiveModel::Serializer
   attributes :id, :name, :background, :race, :profession, :alignment, :experience, :image_url, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma
+
+  has_one :character_profile
 end

f9c50e374e8f550b9c00760a3f02ad660b65118b
Daniel Calvin <arockne2003@yahoo.com>
2022-06-05 16:52:01 -0700

    add scroll area for appearance and backstory

---
+++ client/src/features/character/CharacterBiography.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Divider, ScrollArea, Text, Title } from '@mantine/core'
+
+function CharacterBiography({ character }) {
+  console.log(character)
+  return (
+    <>
+      <Title order={4}>Appearance</Title>
+      <Divider />
+      <ScrollArea style={{ height: 200 }} offsetScrollbars>
+        <Text>{character?.character_profile.appearance}</Text>
+      </ScrollArea>
+      <br />
+      <Title order={4}>BackStory</Title>
+      <Divider />
+      <ScrollArea style={{ height: 200 }} offsetScrollbars>
+        <Text>{character?.character_profile.backstory}</Text>
+      </ScrollArea>
+    </>
+  )
+}
+
+export default CharacterBiography

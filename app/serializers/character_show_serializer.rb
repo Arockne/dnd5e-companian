df4d4d9e6b5e86feeec722afb1a2ba7d60911f84
Daniel Calvin <arockne2003@yahoo.com>
2022-06-05 15:19:54 -0700

    add campaign association

--- app/serializers/character_show_serializer.rb
+++ app/serializers/character_show_serializer.rb
@@ -1,5 +1,6 @@
 class CharacterShowSerializer < ActiveModel::Serializer
   attributes :id, :name, :background, :race, :profession, :alignment, :experience, :image_url, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma
 
+  belongs_to :campaign
   has_one :character_profile
 end

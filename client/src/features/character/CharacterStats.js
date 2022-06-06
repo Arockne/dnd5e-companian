a307bdfae761e588d6dcfbb7dc55425a280f76bf
Daniel Calvin <arockne2003@yahoo.com>
2022-06-06 16:04:21 -0700

    change stats to not be abbreviated

--- client/src/features/character/CharacterStats.js
+++ client/src/features/character/CharacterStats.js
@@ -4,12 +4,12 @@ import React from 'react'
 function CharacterStats({ character }) {
   return (
     <Stack align="flex-start" spacing={0}>
-      <Text size="xs">Str: {character?.strength}</Text>
-      <Text size="xs">Dex: {character?.dexterity}</Text>
-      <Text size="xs">Con: {character?.constitution}</Text>
-      <Text size="xs">Int: {character?.intelligence}</Text>
-      <Text size="xs">Wis: {character?.wisdom}</Text>
-      <Text size="xs">Cha: {character?.charisma}</Text>
+      <Text size="xs">Strength: {character?.strength}</Text>
+      <Text size="xs">Dexterity: {character?.dexterity}</Text>
+      <Text size="xs">Constitution: {character?.constitution}</Text>
+      <Text size="xs">Intelligence: {character?.intelligence}</Text>
+      <Text size="xs">Wisdom: {character?.wisdom}</Text>
+      <Text size="xs">Charisma: {character?.charisma}</Text>
     </Stack>
   )
 }

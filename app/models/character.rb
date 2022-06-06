db2183a528cbb4ddef15bc24b594dad5c3d8f125
Daniel Calvin <arockne2003@yahoo.com>
2022-06-06 16:34:30 -0700

    add age validation

--- app/models/character.rb
+++ app/models/character.rb
@@ -9,6 +9,7 @@ class Character < ApplicationRecord
   validates :profession, presence: true
   validates :alignment, inclusion: { in: [ 'Lawful Good', 'Neutral Good', 'Chaotic Good', 'Lawful Neutral', 'Neutral', 'Chaotic Neutral', 'Lawful Evil', 'Neutral Evil', 'Chaotic Evil' ]}
 
+  validates :age, numericality: { greater_than: 0 }
   validates :experience, numericality: { greater_than_or_equal_to: 0 }
   validates :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, numericality: { greater_than_or_equal_to: 0 }, on: :update
   validates :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, numericality: { greater_than_or_equal_to: 3 }, on: :create

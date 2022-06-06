f3ef2d96a69a25373ea66a9a9c8fe3ab5fc0d6dd
Daniel Calvin <arockne2003@yahoo.com>
2022-06-05 18:23:31 -0700

    add character stats

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,5 +1,5 @@
 class CharacterSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image_url, :profession
+  attributes :id, :name, :image_url, :profession, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma
 
   belongs_to :campaign
 end

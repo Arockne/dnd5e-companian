f33b92e64f89218379fcc7e81a64a208598e99f1
Daniel Calvin <arockne2003@yahoo.com>
2022-06-07 19:34:25 -0700

    add default value of 0 to abilityModifier

--- client/src/features/character/CharacterStat.js
+++ client/src/features/character/CharacterStat.js
@@ -2,7 +2,7 @@ import { Card, Group, Text, ThemeIcon } from '@mantine/core'
 import React from 'react'
 
 function CharacterStat({ name, stat }) {
-  function abilityModifier(stat) {
+  function abilityModifier(stat = 0) {
     const modifier = Math.floor((stat - 10) / 2)
     return modifier > 0 ? `+${modifier}` : modifier
   }

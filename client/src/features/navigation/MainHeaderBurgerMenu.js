612aaadfdba64a4fc3eab1c9ebe7694a835a96b5
Daniel Calvin <arockne2003@yahoo.com>
2022-06-10 13:16:58 -0700

    add labels to navigation sections

--- client/src/features/navigation/MainHeaderBurgerMenu.js
+++ client/src/features/navigation/MainHeaderBurgerMenu.js
@@ -1,11 +1,27 @@
-import { Anchor, Avatar, Group, Text, Button } from '@mantine/core'
+import {
+  Anchor,
+  Avatar,
+  Group,
+  Text,
+  Button,
+  Divider,
+  Space,
+  Stack,
+  Container,
+} from '@mantine/core'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Book2, Logout, Settings, Swords } from 'tabler-icons-react'
 
 function MainHeaderBurgerMenu({ user, toggleOpened }) {
   return (
-    <Group direction="column" position="center" spacing={0} radius="lg" grow>
+    <Group
+      direction="column"
+      position="center"
+      spacing={0}
+      grow
+      style={{ width: 500, margin: '0 auto' }}
+    >
       <Group spacing={7}>
         <Avatar src={user.image} alt={user.name} radius="xl" size={20} />
         <Text weight={500} size="sm" sx={{ lineHeight: 1 }} mr={3}>
@@ -16,6 +32,7 @@ function MainHeaderBurgerMenu({ user, toggleOpened }) {
         component={Link}
         to={'/campaigns/create'}
         onClick={() => toggleOpened()}
+        radius={0}
       >
         Create Campaign
       </Button>
@@ -23,21 +40,30 @@ function MainHeaderBurgerMenu({ user, toggleOpened }) {
         component={Link}
         to={'/campaigns/search'}
         onClick={() => toggleOpened()}
+        radius={0}
       >
         Search Campaigns
       </Button>
+      <Divider label="User Resources" labelPosition="center" />
       <Button
         component={Link}
         to={'/characters'}
         onClick={() => toggleOpened()}
+        radius={0}
       >
         <Book2 size={14} />
         Characters
       </Button>
-      <Button component={Link} to={'/campaigns'} onClick={() => toggleOpened()}>
+      <Button
+        component={Link}
+        to={'/campaigns'}
+        onClick={() => toggleOpened()}
+        radius={0}
+      >
         <Swords size={14} />
         Campaigns
       </Button>
+      <Divider label="Settings" labelPosition="center" />
     </Group>
   )
 }

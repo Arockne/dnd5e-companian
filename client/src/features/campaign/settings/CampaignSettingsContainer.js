282bc3697b7753b64a30ba7df23faa6972418c52
Daniel Calvin <arockne2003@yahoo.com>
2022-06-11 12:53:21 -0700

    replace Container with react fragment

--- client/src/features/campaign/settings/CampaignSettingsContainer.js
+++ client/src/features/campaign/settings/CampaignSettingsContainer.js
@@ -1,4 +1,4 @@
-import { Container, Space } from '@mantine/core'
+import { Space } from '@mantine/core'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import CampaignDeleteContainer from './CampaignDeleteContainer'
@@ -9,13 +9,13 @@ function CampaignSettingsContainer() {
   const { campaign, status, errors } = useSelector((state) => state.campaign)
 
   return (
-    <Container>
+    <>
       <CampaignUpdateForm campaign={campaign} status={status} errors={errors} />
       <Space h="lg" />
       <CampaignUpdatePasswordForm campaign={campaign} />
       <Space h="lg" />
       <CampaignDeleteContainer campaign={campaign} status={status} />
-    </Container>
+    </>
   )
 }
 

5ae0c06f6ae7b0118b7bd76418c6346eb71e1eff
Daniel Calvin <arockne2003@yahoo.com>
2022-06-10 18:12:56 -0700

    add style and structure

--- client/src/features/campaign/CampaignHeader.js
+++ client/src/features/campaign/CampaignHeader.js
@@ -1,22 +1,108 @@
 import React, { useEffect } from 'react'
-import { Anchor, Grid, Group, Loader, Stack, Tabs, Title } from '@mantine/core'
+import {
+  Anchor,
+  Box,
+  createStyles,
+  Divider,
+  Grid,
+  Group,
+  Loader,
+  Stack,
+  Tabs,
+  Title,
+} from '@mantine/core'
 import { MessageCircle, Photo, Settings } from 'tabler-icons-react'
-import { NavLink, Outlet, useParams } from 'react-router-dom'
+import { Link, NavLink, Outlet, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCampaign } from './campaignSlice'
 
+const useStyles = createStyles((theme) => ({
+  link: {
+    ...theme.fn.focusStyles(),
+    display: 'block',
+    textDecoration: 'none',
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+    lineHeight: 1.2,
+    fontSize: theme.fontSizes.sm,
+    padding: theme.spacing.xs,
+    borderTopRightRadius: theme.radius.sm,
+    borderBottomRightRadius: theme.radius.sm,
+    borderLeft: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+    },
+  },
+
+  linkActive: {
+    fontWeight: 500,
+    borderLeftColor:
+      theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 6 : 7],
+    color:
+      theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 2 : 7],
+
+    '&, &:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.fn.rgba(theme.colors[theme.primaryColor][9], 0.25)
+          : theme.colors[theme.primaryColor][0],
+    },
+  },
+}))
+
 export function CampaignHeader({ campaign, owner }) {
+  const { classes, cx } = useStyles()
+
   return (
-    <Grid grow>
+    <Grid grow style={{ margin: '0 150px' }}>
       <Grid.Col span={1}>
         <Stack>
-          <NavLink to={`/campaigns/${campaign?.id}`}>Overview</NavLink>
-          <NavLink to={`/campaigns/${campaign?.id}`}>Players</NavLink>
-          <NavLink to={`/campaigns/${campaign?.id}`}>Messages</NavLink>
+          <NavLink
+            to={`/campaigns/${campaign?.id}`}
+            className={({ isActive }) =>
+              cx(classes.link, { [classes.linkActive]: isActive })
+            }
+            end={`/campaigns/${campaign?.id}`}
+          >
+            Overview
+          </NavLink>
+          <NavLink
+            to={`/campaigns/${campaign?.id}/players`}
+            className={({ isActive }) =>
+              cx(classes.link, { [classes.linkActive]: isActive })
+            }
+          >
+            Players
+          </NavLink>
+          <NavLink
+            to={`/campaigns/${campaign?.id}/messages`}
+            className={({ isActive }) =>
+              cx(classes.link, { [classes.linkActive]: isActive })
+            }
+          >
+            Messages
+          </NavLink>
           {owner ? (
             <>
-              <NavLink to={`/campaigns/${campaign?.id}`}>Logs</NavLink>
-              <NavLink to={`/campaigns/${campaign?.id}/settings`}>
+              <NavLink
+                to={`/campaigns/${campaign?.id}/logs`}
+                className={({ isActive }) =>
+                  cx(classes.link, { [classes.linkActive]: isActive })
+                }
+              >
+                Logs
+              </NavLink>
+              <NavLink
+                to={`/campaigns/${campaign?.id}/settings`}
+                className={({ isActive }) =>
+                  cx(classes.link, { [classes.linkActive]: isActive })
+                }
+              >
                 Settings
               </NavLink>
             </>

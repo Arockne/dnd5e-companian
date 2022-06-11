960bc474d57ad012fbbb2aa21cefdd5d9a582c39
Daniel Calvin <arockne2003@yahoo.com>
2022-06-10 18:33:44 -0700

    change border direction to be on right

--- client/src/features/campaign/CampaignHeader.js
+++ client/src/features/campaign/CampaignHeader.js
@@ -25,9 +25,9 @@ const useStyles = createStyles((theme) => ({
     lineHeight: 1.2,
     fontSize: theme.fontSizes.sm,
     padding: theme.spacing.xs,
-    borderTopRightRadius: theme.radius.sm,
-    borderBottomRightRadius: theme.radius.sm,
-    borderLeft: `1px solid ${
+    borderTopLeftRadius: theme.radius.sm,
+    borderBottomLeftRadius: theme.radius.sm,
+    borderRight: `1px solid ${
       theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
     }`,
 
@@ -40,8 +40,7 @@ const useStyles = createStyles((theme) => ({
   },
 
   linkActive: {
-    fontWeight: 500,
-    borderLeftColor:
+    borderRightColor:
       theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 6 : 7],
     color:
       theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 2 : 7],

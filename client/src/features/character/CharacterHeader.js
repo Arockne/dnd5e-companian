158a2077b3951c176472ef896c146141132c98bf
Daniel Calvin <arockne2003@yahoo.com>
2022-06-11 19:28:54 -0700

    implement styling and structure

--- client/src/features/character/CharacterHeader.js
+++ client/src/features/character/CharacterHeader.js
@@ -1,18 +1,108 @@
-import { Grid, Group, Stack, Title } from '@mantine/core'
+import { createStyles, Grid, Group, Navbar, Stack, Title } from '@mantine/core'
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 
+const useStyles = createStyles((theme, _params, getRef) => {
+  const icon = getRef('icon')
+  return {
+    header: {
+      paddingBottom: theme.spacing.md,
+      marginBottom: theme.spacing.md * 1.5,
+      borderBottom: `1px solid ${
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[4]
+          : theme.colors.gray[2]
+      }`,
+    },
+
+    footer: {
+      paddingTop: theme.spacing.md,
+      marginTop: theme.spacing.md,
+      borderTop: `1px solid ${
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[4]
+          : theme.colors.gray[2]
+      }`,
+    },
+
+    link: {
+      ...theme.fn.focusStyles(),
+      display: 'flex',
+      alignItems: 'center',
+      textDecoration: 'none',
+      fontSize: theme.fontSizes.sm,
+      color:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[1]
+          : theme.colors.gray[7],
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 500,
+
+      '&:hover': {
+        backgroundColor:
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+        [`& .${icon}`]: {
+          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        },
+      },
+    },
+
+    linkIcon: {
+      ref: icon,
+      color:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[2]
+          : theme.colors.gray[6],
+      marginRight: theme.spacing.sm,
+    },
+
+    linkActive: {
+      '&, &:hover': {
+        backgroundColor:
+          theme.colorScheme === 'dark'
+            ? theme.fn.rgba(theme.colors[theme.primaryColor][8], 0.25)
+            : theme.colors[theme.primaryColor][0],
+        color:
+          theme.colorScheme === 'dark'
+            ? theme.white
+            : theme.colors[theme.primaryColor][7],
+        [`& .${icon}`]: {
+          color:
+            theme.colors[theme.primaryColor][
+              theme.colorScheme === 'dark' ? 5 : 7
+            ],
+        },
+      },
+    },
+  }
+})
+
 function CharacterHeader({ character }) {
+  const { classes, cx } = useStyles()
   return (
     <Grid grow>
       <Grid.Col span={1}>
-        <Stack>
-          <NavLink
-            to={`/campaigns/${character?.campaign.id}/characters/${character?.id}`}
-          >
-            Overview
-          </NavLink>
-        </Stack>
+        <Navbar height={700} width={{ sm: 300 }} p="md">
+          <Navbar.Section grow>
+            <Group className={classes.header} position="apart">
+              {character?.name}
+            </Group>
+            <NavLink
+              to={`/campaigns/${character?.campaign.id}/characters/${character?.id}`}
+              className={({ isActive }) =>
+                cx(classes.link, { [classes.linkActive]: isActive })
+              }
+              end={`/campaigns/${character?.campaign.id}/characters/${character?.id}`}
+            >
+              Overview
+            </NavLink>
+          </Navbar.Section>
+        </Navbar>
       </Grid.Col>
       <Grid.Col span={9}>
         <Outlet />

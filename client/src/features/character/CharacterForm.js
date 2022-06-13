cc02ff9aac59c0ccd9ebf684e404b4190c54ef21
Daniel Calvin <arockne2003@yahoo.com>
2022-06-12 17:44:09 -0700

    add name attribute to elements

--- client/src/features/character/CharacterForm.js
+++ client/src/features/character/CharacterForm.js
@@ -10,27 +10,27 @@ import React from 'react'
 function CharacterForm() {
   return (
     <form>
-      <TextInput label="Name" />
-      <TextInput label="Image" />
-      <ColorInput label="Eyes" />
-      <ColorInput label="Hair" />
-      <ColorInput label="Skin" />
-      <Textarea label="Appearance" />
-      <Textarea label="Backstory" />
-      {/* <Select label="Race" />
-      <Select label="Class" />
-      <Select label="Background" />
-      <Select label="Alignment" />
-      <Select label="Gender" /> */}
-      <NumberInput label="Age" />
-      <NumberInput label="Height" />
-      <NumberInput label="Weight" />
-      <NumberInput label="Strength" />
-      <NumberInput label="Dexterity" />
-      <NumberInput label="Constitution" />
-      <NumberInput label="Intelligence" />
-      <NumberInput label="Wisdom" />
-      <NumberInput label="Charisma" />
+      <TextInput label="Name" name="name" />
+      <TextInput label="Image" name="image_url" />
+      <ColorInput label="Eyes" name="eyes" />
+      <ColorInput label="Hair" name="hair" />
+      <ColorInput label="Skin" name="skin" />
+      <Textarea label="Appearance" name="appearance" />
+      <Textarea label="Backstory" name="backstory" />
+      {/* <Select label="Race" name="race" />
+      <Select label="Class" name="profession" />
+      <Select label="Background" name="background" />
+      <Select label="Alignment" name="alignment" />
+      <Select label="Gender" name="gender" /> */}
+      <NumberInput label="Age" name="age" />
+      <NumberInput label="Height" name="height" />
+      <NumberInput label="Weight" name="weight" />
+      <NumberInput label="Strength" name="strength" />
+      <NumberInput label="Dexterity" name="dexterity" />
+      <NumberInput label="Constitution" name="constitution" />
+      <NumberInput label="Intelligence" name="intelligence" />
+      <NumberInput label="Wisdom" name="wisdom" />
+      <NumberInput label="Charisma" name="charisma" />
     </form>
   )
 }

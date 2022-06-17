b44dc070358e24b60a1410461ea41b99d8eb4174
Daniel Calvin <arockne2003@yahoo.com>
2022-06-17 14:18:01 -0700

    implement image validation

--- app/models/character.rb
+++ app/models/character.rb
@@ -8,6 +8,9 @@ class Character < ApplicationRecord
   validates :profession, presence: true
   validates :alignment, inclusion: { in: [ 'Lawful Good', 'Neutral Good', 'Chaotic Good', 'Lawful Neutral', 'Neutral', 'Chaotic Neutral', 'Lawful Evil', 'Neutral Evil', 'Chaotic Evil' ]}
 
+  validates :image_url, presence: true
+  validate :image
+
   validates :age, numericality: { greater_than: 0 }
   validates :experience, numericality: { greater_than_or_equal_to: 0 }
   validates :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, numericality: { greater_than_or_equal_to: 0 }, on: :update
@@ -15,4 +18,13 @@ class Character < ApplicationRecord
   validates :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, numericality: {
     less_than_or_equal_to: 30
   }
+
+  def image
+    if image_url.present?
+      unless /(http(s?):)([\/|.|\w|\s|-])*\.(?:jpg|gif|png)/i.match(image_url)
+        errors.add(:image_url, "is not valid")
+      end
+    end
+  end
+
 end

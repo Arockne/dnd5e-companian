1f8a1002d0a7e46fe1b899f726f5fd5441b3a660
Daniel Calvin <arockne2003@yahoo.com>
2022-06-17 18:46:35 -0700

    fix structure

--- client/src/features/campaign/CampaignOverview.js
+++ client/src/features/campaign/CampaignOverview.js
@@ -1,4 +1,4 @@
-import { Container, Group, Image, Text } from '@mantine/core'
+import { Container, Group, Image, ScrollArea, Text } from '@mantine/core'
 import React from 'react'
 import { useSelector } from 'react-redux'
 
@@ -6,19 +6,17 @@ function CampaignOverview() {
   const { campaign } = useSelector((state) => state.campaign)
 
   return (
-    <Group direction="row">
-      <Container>
-        <Image
-          withPlaceholder={true}
-          src={campaign?.image_url}
-          alt="campaign image"
-          width={400}
-          height={225}
-        />
-      </Container>
-      <Container>
+    <Group direction="row" align="start" position="center" spacing="xl">
+      <Image
+        withPlaceholder={true}
+        src={campaign?.image_url}
+        alt="campaign image"
+        width={400}
+        height={225}
+      />
+      <ScrollArea style={{ height: 225, width: 400 }}>
         <Text>{campaign?.setting}</Text>
-      </Container>
+      </ScrollArea>
     </Group>
   )
 }

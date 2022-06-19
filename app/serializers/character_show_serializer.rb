ef1091fcb4667280c94e8956afa61699fa451da9
Daniel Calvin <arockne2003@yahoo.com>
2022-06-19 12:50:33 -0700

    add visible attribute

--- app/serializers/character_show_serializer.rb
+++ app/serializers/character_show_serializer.rb
@@ -1,5 +1,5 @@
 class CharacterShowSerializer < ActiveModel::Serializer
-  attributes :id, :name, :background, :race, :klass, :alignment, :experience, :image_url, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :age, :height, :weight, :eyes, :skin, :hair, :gender, :appearance, :backstory
+  attributes :id, :name, :background, :race, :klass, :alignment, :experience, :image_url, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :age, :height, :weight, :eyes, :skin, :hair, :gender, :appearance, :backstory, :visible
 
   belongs_to :campaign
   belongs_to :user

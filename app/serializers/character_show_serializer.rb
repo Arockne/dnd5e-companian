2770ab377dda41ef829089457f7ae9f3bd40daf9
Daniel Calvin <arockne2003@yahoo.com>
2022-06-19 12:18:45 -0700

    add user association to serialized data

--- app/serializers/character_show_serializer.rb
+++ app/serializers/character_show_serializer.rb
@@ -2,4 +2,5 @@ class CharacterShowSerializer < ActiveModel::Serializer
   attributes :id, :name, :background, :race, :klass, :alignment, :experience, :image_url, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :age, :height, :weight, :eyes, :skin, :hair, :gender, :appearance, :backstory
 
   belongs_to :campaign
+  belongs_to :user
 end

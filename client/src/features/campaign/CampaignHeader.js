6bc8c691d6ff58e67abc38a4d707e1ce103d590e
Daniel Calvin <arockne2003@yahoo.com>
2022-06-20 09:32:58 -0700

    style and structure leave campaign button

--- client/src/features/campaign/CampaignHeader.js
+++ client/src/features/campaign/CampaignHeader.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import {
   Anchor,
   Box,
+  Button,
   createStyles,
   Divider,
   Grid,
@@ -19,6 +20,8 @@ import {
   Photo,
   Settings,
   SwitchHorizontal,
+  TransferOut,
+  AlertTriangle,
 } from 'tabler-icons-react'
 import { Link, NavLink, Outlet, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
@@ -101,6 +104,34 @@ const useStyles = createStyles((theme, _params, getRef) => {
         },
       },
     },
+
+    leaveButton: {
+      ...theme.fn.focusStyles(),
+      display: 'flex',
+      alignItems: 'center',
+      textDecoration: 'none',
+      fontSize: theme.fontSizes.sm,
+      color:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[1]
+          : theme.colors.red[7],
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 500,
+      height: 45,
+
+      '&:hover': {
+        backgroundColor:
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+        [`& .${icon}`]: {
+          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        },
+      },
+    },
   }
 })
 
@@ -167,6 +198,12 @@ export function CampaignHeader({ campaign, owner }) {
               </NavLink>
             </>
           ) : null}
+        </Navbar.Section>
+        <Navbar.Section className={classes.footer}>
+          <Button variant="subtle" className={classes.leaveButton}>
+            <AlertTriangle className={classes.linkIcon} />
+            <span>Leave Campaign</span>
+          </Button>
         </Navbar.Section>
       </Navbar>
       <Group direction="column" grow>

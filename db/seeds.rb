8068d544f3666ecf22f6b46941e74f4af0533852
Daniel Calvin <arockne2003@yahoo.com>
2022-06-20 08:17:19 -0700

    reorganize file structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'seeding file 🌱'
 
-user_1 = User.create(
-  username: 'bob', 
-  email: 'bob@gmail.com', 
-  password: 'test1234'
-)
-user_2 = User.create(
-  username: 'karen', 
-  email: 'karen@gmail.com',
-  password: 'test1234'
-)
-user_3 = User.create(
-  username: 'sam', 
-  email: 'sam@gmail.com',
-  password: 'test1234'
-)
-
-users = [user_1, user_2, user_3]
-
-puts 'Creating campaigns'
-
 campaign_images = [
   'https://www.designyourway.net/art/wp-content/uploads/2019/06/berube-jonathan-berube-art-director-matte-192-700x374.jpg',
   'https://wallpaperaccess.com/full/2037483.jpg',
@@ -47,6 +27,44 @@ campaign_images = [
   'https://cdna.artstation.com/p/assets/images/images/050/201/306/large/james-abels-dark-fantasytimetoarchitecture-1-1-upscaled-art-scale-6-00x.jpg?1654280200',
   'https://wallpaperforu.com/wp-content/uploads/2020/07/space-wallpaper-2007071541573-scaled.jpg'
 ]
+
+character_images = [
+  'https://i.pinimg.com/originals/c7/f7/b2/c7f7b2e00646a1a1e1f625ec504e0530.jpg',
+  'https://i.pinimg.com/474x/15/2f/ae/152fae1f947c5a6cc69213c0e44c4ba6.jpg',
+  'https://i.pinimg.com/474x/dc/c5/29/dcc529ddfe413d62e77be948aa1ccbaa.jpg',
+  'https://i.pinimg.com/originals/c1/7e/0a/c17e0a1467b8ea90f32b681015fe351b.jpg',
+  'https://i.pinimg.com/originals/97/ba/37/97ba379d249f5139628fa9a42e55140b.png',
+  'https://i.pinimg.com/474x/25/db/91/25db9125dd421502375b1db3530109bf.jpg',
+  'https://i.pinimg.com/originals/c5/cb/d4/c5cbd4ac240bf7e7937851a9cb53da4e.jpg',
+  'https://i.pinimg.com/originals/da/34/31/da343123d7e23bcdfdf2f4a207cfe7eb.jpg',
+  'https://i.pinimg.com/736x/6f/46/34/6f46344fa378c5cf1c43d2024fff20bc.jpg',
+  'https://i.pinimg.com/736x/9b/ce/40/9bce400b4660c1918b8e0c0eb725bfce.jpg',
+  'https://i.pinimg.com/564x/5a/d1/a6/5ad1a6a09ba613d248d56a098c8792ad.jpg',
+  'https://i.pinimg.com/736x/48/53/91/485391ab775f002594475afb2755ea73.jpg',
+  'https://i.pinimg.com/736x/25/04/f6/2504f618ac3ee1230f2eadd3f9db2fa5.jpg',
+  'https://i.pinimg.com/564x/95/2e/85/952e858c7b2938ab9a75313ff21c7a0c.jpg',
+  'https://i.pinimg.com/736x/9c/85/a4/9c85a458e0b15010040e537afbaf28f2--character-portraits-character-art.jpg'
+]
+
+user_1 = User.create(
+  username: 'bob', 
+  email: 'bob@gmail.com', 
+  password: 'test1234'
+)
+user_2 = User.create(
+  username: 'karen', 
+  email: 'karen@gmail.com',
+  password: 'test1234'
+)
+user_3 = User.create(
+  username: 'sam', 
+  email: 'sam@gmail.com',
+  password: 'test1234'
+)
+
+users = [user_1, user_2, user_3]
+
+puts 'Creating campaigns'
 
 campaign_1 = Campaign.create!(
   name: 'Knights of the Round Table', 
@@ -83,21 +101,6 @@ user_1.campaign_users.create!(campaign: campaign_2)
   filtered_users.each_entry { |user| user.campaign_users.create!(campaign: campaign)}
 end
 
-20.times do 
-  setting = []
-  8.times do
-    setting.push(Faker::Quotes::Shakespeare.unique.king_richard_iii_quote)
-  end
-  Campaign.create(
-    name: Faker::Games::Zelda.location, 
-    image_url: campaign_images[rand(0...campaign_images.length)],
-    setting: setting.join(' '),
-    owner: users[rand(0..2)], 
-    password: 'test1234',
-  )
-  Faker::Quotes::Shakespeare.unique.clear
-end
-
 def roll_stat()
   dice = []
   dice_amount = 4
@@ -108,23 +111,6 @@ def roll_stat()
 end
 
 puts 'Creating characters'
-character_images = [
-  'https://i.pinimg.com/originals/c7/f7/b2/c7f7b2e00646a1a1e1f625ec504e0530.jpg',
-  'https://i.pinimg.com/474x/15/2f/ae/152fae1f947c5a6cc69213c0e44c4ba6.jpg',
-  'https://i.pinimg.com/474x/dc/c5/29/dcc529ddfe413d62e77be948aa1ccbaa.jpg',
-  'https://i.pinimg.com/originals/c1/7e/0a/c17e0a1467b8ea90f32b681015fe351b.jpg',
-  'https://i.pinimg.com/originals/97/ba/37/97ba379d249f5139628fa9a42e55140b.png',
-  'https://i.pinimg.com/474x/25/db/91/25db9125dd421502375b1db3530109bf.jpg',
-  'https://i.pinimg.com/originals/c5/cb/d4/c5cbd4ac240bf7e7937851a9cb53da4e.jpg',
-  'https://i.pinimg.com/originals/da/34/31/da343123d7e23bcdfdf2f4a207cfe7eb.jpg',
-  'https://i.pinimg.com/736x/6f/46/34/6f46344fa378c5cf1c43d2024fff20bc.jpg',
-  'https://i.pinimg.com/736x/9b/ce/40/9bce400b4660c1918b8e0c0eb725bfce.jpg',
-  'https://i.pinimg.com/564x/5a/d1/a6/5ad1a6a09ba613d248d56a098c8792ad.jpg',
-  'https://i.pinimg.com/736x/48/53/91/485391ab775f002594475afb2755ea73.jpg',
-  'https://i.pinimg.com/736x/25/04/f6/2504f618ac3ee1230f2eadd3f9db2fa5.jpg',
-  'https://i.pinimg.com/564x/95/2e/85/952e858c7b2938ab9a75313ff21c7a0c.jpg',
-  'https://i.pinimg.com/736x/9c/85/a4/9c85a458e0b15010040e537afbaf28f2--character-portraits-character-art.jpg'
-]
 
 Campaign.all.each_entry do |campaign|
     filtered_users = users.filter { |user| user.username != campaign.owner.username }
@@ -170,7 +156,21 @@ Campaign.all.each_entry do |campaign|
   end
 end
 
+puts 'Creating campaigns without players'
 
-
+20.times do 
+  setting = []
+  8.times do
+    setting.push(Faker::Quotes::Shakespeare.unique.king_richard_iii_quote)
+  end
+  Campaign.create(
+    name: Faker::Games::Zelda.location, 
+    image_url: campaign_images[rand(0...campaign_images.length)],
+    setting: setting.join(' '),
+    owner: users[rand(0..2)], 
+    password: 'test1234',
+  )
+  Faker::Quotes::Shakespeare.unique.clear
+end
 
 puts 'finished seeding 🌹'

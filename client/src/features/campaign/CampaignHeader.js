5ae0e37f04a214cc33b059006907bb4a5bcec538
Daniel Calvin <arockne2003@yahoo.com>
2022-06-20 14:08:59 -0700

    refactor to use CampaignLeaveContainer

--- client/src/features/campaign/CampaignHeader.js
+++ client/src/features/campaign/CampaignHeader.js
@@ -28,6 +28,7 @@ import { Link, NavLink, Outlet, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCampaign } from './campaignSlice'
 import CampaignLeaveForm from './CampaignLeaveForm'
+import CampaignLeaveContainer from './CampaignLeaveContainer'
 
 const useStyles = createStyles((theme, _params, getRef) => {
   const icon = getRef('icon')
@@ -106,34 +107,6 @@ const useStyles = createStyles((theme, _params, getRef) => {
         },
       },
     },
-
-    leaveButton: {
-      ...theme.fn.focusStyles(),
-      display: 'flex',
-      alignItems: 'center',
-      textDecoration: 'none',
-      fontSize: theme.fontSizes.sm,
-      color:
-        theme.colorScheme === 'dark'
-          ? theme.colors.dark[1]
-          : theme.colors.red[7],
-      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
-      borderRadius: theme.radius.sm,
-      fontWeight: 500,
-      height: 45,
-
-      '&:hover': {
-        backgroundColor:
-          theme.colorScheme === 'dark'
-            ? theme.colors.dark[6]
-            : theme.colors.gray[0],
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-
-        [`& .${icon}`]: {
-          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-        },
-      },
-    },
   }
 })
 
@@ -204,22 +177,7 @@ export function CampaignHeader({ campaign, owner }) {
         </Navbar.Section>
         {!owner ? (
           <Navbar.Section className={classes.footer}>
-            <Modal
-              centered
-              title={`Leaving ${campaign?.name}?`}
-              opened={openLeaveCampaignForm}
-              onClose={() => setOpenLeaveCampaignForm(false)}
-            >
-              <CampaignLeaveForm campaign={campaign} />
-            </Modal>
-            <Button
-              variant="subtle"
-              className={classes.leaveButton}
-              onClick={() => setOpenLeaveCampaignForm(true)}
-            >
-              <AlertTriangle className={classes.linkIcon} />
-              <span>Leave Campaign</span>
-            </Button>
+            <CampaignLeaveContainer campaign={campaign} />
           </Navbar.Section>
         ) : null}
       </Navbar>

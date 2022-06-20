c32b37b0b52983fce4c81fee5593d0c6459a1274
Daniel Calvin <arockne2003@yahoo.com>
2022-06-20 13:49:51 -0700

    implement requirement for leaving a campaign

--- client/src/features/campaign/CampaignLeaveForm.js
+++ client/src/features/campaign/CampaignLeaveForm.js
@@ -1,19 +1,32 @@
 import { Button, Group, Text, TextInput } from '@mantine/core'
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { client } from '../../api/client'
 
-function CampaignLeaveForm({ currentPlayer }) {
+function CampaignLeaveForm({ campaign }) {
+  const [inputData, setInputData] = useState('')
+
   const navigate = useNavigate()
+
+  const textToMatch = `I want to leave ${campaign.name}`
+  const inputMatchesRequirement = textToMatch === inputData
+
+  const { current_player } = campaign
+
+  function handleInputChange(e) {
+    setInputData(e.target.value)
+  }
+
   async function handleSubmit(e) {
     e.preventDefault()
     const response = await client.delete(
-      `/api/campaigns/${currentPlayer.campaign_id}/campaign_users/${currentPlayer.id}`
+      `/api/campaigns/${current_player.campaign_id}/campaign_users/${current_player.id}`
     )
     if (response.ok) {
       navigate('/')
     }
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <Text size="sm">
@@ -24,9 +37,12 @@ function CampaignLeaveForm({ currentPlayer }) {
         characters affiliated with this campaign. Once you leave there is no
         going back.
       </Text>
-      <TextInput />
+      <Text size="sm">
+        Please type <strong>{textToMatch}</strong> to confirm
+      </Text>
+      <TextInput value={inputData} onChange={handleInputChange} />
       <Group position="center" grow>
-        <Button type="submit">
+        <Button type="submit" disabled={!inputMatchesRequirement}>
           I understand the consequences of my actions
         </Button>
       </Group>

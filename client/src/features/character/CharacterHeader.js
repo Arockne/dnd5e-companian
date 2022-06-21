15c60ca119933c5935c6f854ce82e3b19e950972
Daniel Calvin <arockne2003@yahoo.com>
2022-06-21 10:41:26 -0700

    implement setting link

--- client/src/features/character/CharacterHeader.js
+++ client/src/features/character/CharacterHeader.js
@@ -140,6 +140,15 @@ function CharacterHeader({ character }) {
           >
             Overview
           </NavLink>
+          <NavLink
+            to={`/campaigns/${character?.campaign.id}/characters/${character?.id}/settings`}
+            className={({ isActive }) =>
+              cx(classes.link, { [classes.linkActive]: isActive })
+            }
+            end={`/campaigns/${character?.campaign.id}/characters/${character?.id}`}
+          >
+            Settings
+          </NavLink>
         </Navbar.Section>
         <Navbar.Section className={classes.footer}>
           <NavLink

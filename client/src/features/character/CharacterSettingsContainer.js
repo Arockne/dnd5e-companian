f5016722faae3bf70a3a1b99d0b6d6fbd0d32d41
Daniel Calvin <arockne2003@yahoo.com>
2022-06-21 12:54:19 -0700

    add CharacterDeleteContainer and pass character

--- client/src/features/character/CharacterSettingsContainer.js
+++ client/src/features/character/CharacterSettingsContainer.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import CharacterDeleteContainer from './CharacterDeleteContainer'
 
 function CharacterSettingsContainer() {
   const { character } = useSelector((state) => state.character)
-  return <></>
+  return (
+    <>
+      <CharacterDeleteContainer character={character} />
+    </>
+  )
 }
 
 export default CharacterSettingsContainer

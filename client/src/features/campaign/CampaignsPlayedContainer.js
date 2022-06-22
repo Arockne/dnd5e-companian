3b3b38e1398a65a49a63d4b6b2776fcf8b04d0b9
Daniel Calvin <arockne2003@yahoo.com>
2022-06-22 16:03:24 -0700

    implement CampaignsPlayedContainer

---
+++ client/src/features/campaign/CampaignsPlayedContainer.js
@@ -0,0 +1,25 @@
+import { Container, Title } from '@mantine/core'
+import React, { useEffect, useState } from 'react'
+import CampaignsTable from './CampaignsTable'
+
+function CampaignsPlayedContainer() {
+  const [campaigns, setCampaigns] = useState([])
+  useEffect(() => {
+    fetch('/api/campaigns/currently_played').then(async (r) => {
+      const body = await r.json()
+      if (r.ok) {
+        setCampaigns(body)
+      }
+    })
+  }, [])
+  return (
+    <Container>
+      <Title align="center" order={2}>
+        Created Campaigns
+      </Title>
+      <CampaignsTable campaigns={campaigns} />
+    </Container>
+  )
+}
+
+export default CampaignsPlayedContainer

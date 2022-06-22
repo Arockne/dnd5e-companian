a745222a90593cd670007e1a308d8f573b4aa85f
Daniel Calvin <arockne2003@yahoo.com>
2022-06-22 16:11:14 -0700

    fix fetch request to use correct path

--- client/src/features/campaign/CampaignsPlayedContainer.js
+++ client/src/features/campaign/CampaignsPlayedContainer.js
@@ -5,7 +5,7 @@ import CampaignsTable from './CampaignsTable'
 function CampaignsPlayedContainer() {
   const [campaigns, setCampaigns] = useState([])
   useEffect(() => {
-    fetch('/api/campaigns/currently_played').then(async (r) => {
+    fetch('/api/campaigns/currently_playing').then(async (r) => {
       const body = await r.json()
       if (r.ok) {
         setCampaigns(body)

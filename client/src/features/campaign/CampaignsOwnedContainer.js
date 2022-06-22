255b04d9b962cd900c524a7368dc051b9575e5c9
Daniel Calvin <arockne2003@yahoo.com>
2022-06-22 15:46:41 -0700

    move to campaign directory and implement CampaignsOwnedContainer

---
+++ client/src/features/campaign/CampaignsOwnedContainer.js
@@ -0,0 +1,26 @@
+import { Container, Title } from '@mantine/core'
+import React, { useEffect, useState } from 'react'
+import { client } from '../../api/client'
+import CampaignsTable from './CampaignsTable'
+
+function CampaignsOwnedContainer() {
+  const [campaigns, setCampaigns] = useState({})
+  useEffect(() => {
+    fetch('/campaigns/currently_owned').then(async (r) => {
+      const body = await r.json()
+      if (r.ok) {
+        setCampaigns(body)
+      }
+    })
+  }, [])
+  return (
+    <Container>
+      <Title align="center" order={2}>
+        Created Campaigns
+      </Title>
+      <CampaignsTable campaigns={campaigns} />
+    </Container>
+  )
+}
+
+export default CampaignsOwnedContainer

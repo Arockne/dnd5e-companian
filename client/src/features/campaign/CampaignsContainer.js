98c4050045186fb0dbfdfa8fcfdf3a8948b68f28
Daniel Calvin <arockne2003@yahoo.com>
2022-06-24 04:41:10 -0700

    remove redux dispatch to grab current campaigns

--- client/src/features/campaign/CampaignsContainer.js
+++ client/src/features/campaign/CampaignsContainer.js
@@ -1,29 +1,14 @@
-import { Container, Group, Loader, Stack, Title } from '@mantine/core'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import Loading from '../common/Loading'
-import { getCurrentCampaigns } from './campaignSlice'
+import { Group } from '@mantine/core'
+import React from 'react'
 import CampaignsOwnedContainer from './CampaignsOwnedContainer'
 import CampaignsPlayedContainer from './CampaignsPlayedContainer'
-import CampaignsTable from './CampaignsTable'
 
 function CampaignsContainer() {
-  const dispatch = useDispatch()
-  const {
-    campaigns: { owned_campaigns, campaigns },
-  } = useSelector((state) => state.campaign)
-
-  useEffect(() => {
-    dispatch(getCurrentCampaigns())
-  }, [])
-
-  return campaigns ? (
+  return (
     <Group align="center" position="center" spacing={100} grow>
       <CampaignsOwnedContainer />
       <CampaignsPlayedContainer />
     </Group>
-  ) : (
-    <Loading />
   )
 }
 

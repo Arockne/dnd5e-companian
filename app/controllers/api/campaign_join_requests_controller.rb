2d0df7c718fcc54ccf192f707e2f2e40290f7d56
Daniel Calvin <arockne2003@yahoo.com>
2022-08-20 09:26:24 -0700

    implement CampaignJoinRequests create action

--- app/controllers/api/campaign_join_requests_controller.rb
+++ app/controllers/api/campaign_join_requests_controller.rb
@@ -1,8 +1,7 @@
 class Api::CampaignJoinRequestsController < ApplicationController
   
   def create
-    byebug
-    request = CampaignJoinRequest.create(request_params)
+    request = CampaignJoinRequest.create(user: current_user, campaign_id: params[:campaign_id])
     render json: request, status: :ok  
   end
 end

4a3f38efce25c9797726ff4c24d8d0c3b675a14a
Daniel Calvin <arockne2003@yahoo.com>
2022-08-20 10:18:52 -0700

    implement render_unproccessable_entity to catch invalid request

--- app/controllers/api/campaign_join_requests_controller.rb
+++ app/controllers/api/campaign_join_requests_controller.rb
@@ -1,7 +1,14 @@
 class Api::CampaignJoinRequestsController < ApplicationController
-  
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
   def create
-    request = CampaignJoinRequest.create(user: current_user, campaign_id: params[:campaign_id])
+    request = CampaignJoinRequest.create!(user: current_user, campaign_id: params[:campaign_id])
     render json: request, status: :ok  
+  end
+
+  private
+  
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 end

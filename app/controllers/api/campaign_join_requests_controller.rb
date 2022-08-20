2f5fb17337cbb2c0f71714d434523aeace0c0f43
Daniel Calvin <arockne2003@yahoo.com>
2022-08-20 12:02:34 -0700

    implement case where the owner of the campaign cannot request to join their own campaign

--- app/controllers/api/campaign_join_requests_controller.rb
+++ app/controllers/api/campaign_join_requests_controller.rb
@@ -2,15 +2,30 @@ class Api::CampaignJoinRequestsController < ApplicationController
 rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
+  before_action :authorize_join_request, only: [:create]
+
   def create
     request = CampaignJoinRequest.create!(user: current_user, campaign_id: params[:campaign_id])
     render json: request, status: :ok  
   end
 
   private
-  
+
   def render_unprocessable_entity(invalid)
+    byebug
     render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def authorize_join_request
+    render json: { errors: ['Cannot request to join as the owner'] }, status: :unprocessable_entity if campaign_owner?
+  end
+
+  def campaign
+    @campaign ||= Campaign.find(params[:campaign_id])
+  end
+
+  def campaign_owner?
+    current_user == campaign.owner
   end
 
   def render_not_found

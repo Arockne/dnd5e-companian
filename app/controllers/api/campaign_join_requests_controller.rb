8e74fe0b2f1fa5104a0073d27d71018a324d45b6
Daniel Calvin <arockne2003@yahoo.com>
2022-08-20 10:51:30 -0700

    implement render_not_found

--- app/controllers/api/campaign_join_requests_controller.rb
+++ app/controllers/api/campaign_join_requests_controller.rb
@@ -1,5 +1,6 @@
 class Api::CampaignJoinRequestsController < ApplicationController
 rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def create
     request = CampaignJoinRequest.create!(user: current_user, campaign_id: params[:campaign_id])
@@ -10,5 +11,9 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
   
   def render_unprocessable_entity(invalid)
     render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def render_not_found
+    render json: { errors: 'The campaign you are requesting to join is not of this realm'}, status: :not_found
   end
 end

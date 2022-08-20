38fb532c81c25222f7e23bb0ff42afa12349c936
Daniel Calvin <arockne2003@yahoo.com>
2022-08-20 10:09:14 -0700

    add validation to where user cannot send the same request multiple times

--- app/models/campaign_join_request.rb
+++ app/models/campaign_join_request.rb
@@ -1,4 +1,6 @@
 class CampaignJoinRequest < ApplicationRecord
   belongs_to :user
   belongs_to :campaign
+
+  validates :campaign, uniqueness: { scope: :user, message: 'join request has been sent' }
 end

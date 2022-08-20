6387e0b046ffddd7e20ba3959f62717fc1b9e67c
Daniel Calvin <arockne2003@yahoo.com>
2022-08-20 08:40:04 -0700

    implement create resource and request_params

--- app/controllers/api/campaign_join_requests_controller.rb
+++ app/controllers/api/campaign_join_requests_controller.rb
@@ -1,2 +1,12 @@
 class Api::CampaignJoinRequestsController < ApplicationController
+  
+  def create
+    request = CampaignJoinRequest.create(request_params)
+    render json: request, status: :ok  
+  end
+
+
+  def request_params
+    params.permit(:user_id, :campaign_id)
+  end
 end

306cfea3aa708f52eb4d379374c4053bce1bb6b2
Daniel Calvin <arockne2003@yahoo.com>
2022-08-20 19:20:38 -0700

    add has_many campaign_join_requests relationship

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,6 +3,7 @@ class Campaign < ApplicationRecord
   has_many :campaign_users, dependent: :destroy
   has_many :users, through: :campaign_users
   has_many :characters, dependent: :destroy
+  has_many :campaign_join_requests
 
   validates :image_url, presence: true, format: { with: /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/i, message: 'is not valid'}
   validates :name, presence: true, length: { maximum: 30 }, uniqueness: { case_sensitive: false }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :campaign_users, dependent: :destroy
   has_many :campaigns, through: :campaign_users
   has_many :characters, dependent: :destroy
+  has_many :campaign_join_requests
 
   #need to create a custom query for getting all current characters that the current_user has a membership for campaigns. This query method wont grab the characters previously created in campaigns. IF we want the characters that we left we would need to rejoin whatever campaign we were part of. If the campaign owner did not delete the characters of course.
 

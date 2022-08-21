1d4c139ccb85c9e999eef4146abd33bff5139fca
Daniel Calvin <arockne2003@yahoo.com>
2022-08-21 10:44:04 -0700

    change uniqueness message to "join request is pending approval"

--- app/models/campaign_join_request.rb
+++ app/models/campaign_join_request.rb
@@ -2,5 +2,5 @@ class CampaignJoinRequest < ApplicationRecord
   belongs_to :user
   belongs_to :campaign
 
-  validates :campaign, uniqueness: { scope: :user, message: 'join request has been sent' }
+  validates :campaign, uniqueness: { scope: :user, message: 'join request is pending approval' }
 end

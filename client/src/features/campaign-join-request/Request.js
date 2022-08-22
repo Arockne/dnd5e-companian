7ab091ee33d13aa7c2185724cabfeebd94f6b005
Daniel Calvin <arockne2003@yahoo.com>
2022-08-21 18:15:57 -0700

    fix using correct properties from user who sent request

--- client/src/features/campaign-join-request/Request.js
+++ client/src/features/campaign-join-request/Request.js
@@ -1,7 +1,27 @@
+import { Avatar, Button, Group, Text } from '@mantine/core'
 import React from 'react'
 
-function Request() {
-  return <></>
+function Request({ user }) {
+  return (
+    <tr>
+      <td>
+        <Group spacing="sm">
+          <Avatar size={40} src={user.image_url} radius={40} />
+          <div>
+            <Text size="sm" weight={500}>
+              {user.username}
+            </Text>
+          </div>
+        </Group>
+      </td>
+      <td>
+        <Group position="right">
+          <Button>Accept</Button>
+          <Button>Deny</Button>
+        </Group>
+      </td>
+    </tr>
+  )
 }
 
 export default Request

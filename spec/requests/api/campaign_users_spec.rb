d73919c4a021e227cbdc5c296797ed40bafd1f1a
Daniel Calvin <arockne2003@yahoo.com>
2022-10-01 08:45:58 -0700

    remove testing for joining a campaign with a password

--- spec/requests/api/campaign_users_spec.rb
+++ spec/requests/api/campaign_users_spec.rb
@@ -31,7 +31,6 @@ RSpec.describe "Api::CampaignUsers", type: :request do
       image_url: 'https://cdnb.artstation.com/p/assets/images/images/023/999/093/large/gregory-nunkovics-dark-fantasy-landscape.jpg?1580994868',
       setting: 'Somewhere in Camelot', 
       owner: user_1, 
-      password: 'king1234'
     )
   end
 
@@ -40,8 +39,7 @@ RSpec.describe "Api::CampaignUsers", type: :request do
       name: 'Star Wards', 
       image_url: 'https://cdnb.artstation.com/p/assets/images/images/023/999/093/large/gregory-nunkovics-dark-fantasy-landscape.jpg?1580994868',
       setting: 'In a hospital far far away', 
-      owner: user_2, 
-      password: 'test1234'
+      owner: user_2,
     )
   end
 
@@ -50,8 +48,7 @@ RSpec.describe "Api::CampaignUsers", type: :request do
       name: 'The Unending Maze', 
       image_url: 'https://cdnb.artstation.com/p/assets/images/images/023/999/093/large/gregory-nunkovics-dark-fantasy-landscape.jpg?1580994868',
       setting: 'The maze keeps going on and on and on and...', 
-      owner: user_3, 
-      password: 'test1234'
+      owner: user_3,
     )
   end
 
@@ -142,100 +139,6 @@ RSpec.describe "Api::CampaignUsers", type: :request do
       user: user_2,
       campaign: campaign_1
     )
-  end
-
-  describe 'POST /create' do
-    context 'with logged in user' do
-      before do
-        post '/api/login', params: { username: user_1.username, password: user_1.password}
-      end
-      
-      context 'joining a campaign' do
-        context 'with correct password' do
-          let(:campaign_user_params) { { campaign: { password: campaign_3.password } } }
-
-          it 'increases the amount of CampaignUsers by one' do
-            expect { post "/api/campaigns/#{campaign_3.id}/campaign_users", params: campaign_user_params }.to change(CampaignUser, :count).by(1)
-          end
-
-          it 'returns the CampaignUser' do
-            post "/api/campaigns/#{campaign_3.id}/campaign_users", params: campaign_user_params
-            expect(response.body).to include_json({
-              id: a_kind_of(Integer),
-              user_id: user_1.id,
-              campaign_id: campaign_3.id
-            })
-          end
-          it 'returns a status of 201 (Created)' do
-            post "/api/campaigns/#{campaign_3.id}/campaign_users", params: campaign_user_params
-            expect(response).to have_http_status(:created)
-          end
-        end
-        context 'with incorrect password' do
-          let(:incorrect_campaign_user_params) { { campaign: { password: 'thisisnotthepasswordyouarelookingfor' } } }
-
-          it 'returns a status of 401 (Unauthorized)' do
-            post "/api/campaigns/#{campaign_3.id}/campaign_users", params: incorrect_campaign_user_params
-            expect(response).to have_http_status(:unauthorized)
-          end
-
-          it 'returns error messsages' do
-            post "/api/campaigns/#{campaign_3.id}/campaign_users", params: incorrect_campaign_user_params
-            expect(response.body).to include_json({
-              errors: a_kind_of(Array)
-            })
-          end
-        end
-      end
-
-      context 'joining a campaign already a member of' do
-        let(:campaign_user_params) { { campaign: { password: campaign_2.password } } }
-        it 'returns a status of 401 (Unauthorized)' do
-          post "/api/campaigns/#{campaign_2.id}/campaign_users", params: campaign_user_params
-          post "/api/campaigns/#{campaign_2.id}/campaign_users", params: campaign_user_params
-          expect(response).to have_http_status(:unauthorized)
-        end
-        it 'returns error messages' do
-          post "/api/campaigns/#{campaign_2.id}/campaign_users", params: campaign_user_params
-          post "/api/campaigns/#{campaign_2.id}/campaign_users", params: campaign_user_params
-          expect(response.body).to include_json({
-            errors: a_kind_of(Array)
-          })
-        end
-      end
-
-      context 'joining a campaign that does not exist' do
-        let(:campaign_user_params) { { campaign: { password: 'campaigndoesnotexist' } } }
-
-        it 'returns a status of 404 (Not Found)' do
-          post "/api/campaigns/0/campaign_users", params: campaign_user_params
-          expect(response).to have_http_status(:not_found)  
-        end
-
-        it 'returns error messages' do
-          post "/api/campaigns/0/campaign_users", params: campaign_user_params
-          expect(response.body).to include_json({
-            errors: a_kind_of(Array)
-          })
-        end
-      end
-    end
-    
-    context 'without logged in user' do
-      let(:campaign_user_params) { { campaign: { password: campaign_1.password } } }
-
-      it 'returns a status of 401 (Unauthorized)' do
-        post "/api/campaigns/#{campaign_1.id}/campaign_users", params: campaign_user_params
-        expect(response).to have_http_status(:unauthorized)
-      end
-
-      it 'returns error messsages' do
-        post "/api/campaigns/#{campaign_1.id}/campaign_users", params: campaign_user_params
-        expect(response.body).to include_json({
-          errors: a_kind_of(Array)
-        })
-      end
-    end
   end
 
   describe 'DELETE /campaign_users/:id' do

c82a4ef457313825f5603294c55679ea9ded69e4
Daniel Calvin <arockne2003@yahoo.com>
2022-10-01 08:40:27 -0700

    remove password validation tests

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -14,14 +14,12 @@ RSpec.describe Campaign, type: :model do
       image_url: 'https://cdnb.artstation.com/p/assets/images/images/023/999/093/large/gregory-nunkovics-dark-fantasy-landscape.jpg?1580994868',
       setting: 'Somewhere in Camelot', 
       owner: user, 
-      password: 'king1234', 
     )
     Campaign.create(
       name: 'Star Wards', 
       image_url: 'https://cdnb.artstation.com/p/assets/images/images/023/999/093/large/gregory-nunkovics-dark-fantasy-landscape.jpg?1580994868',
       setting: 'In a hospital far far away...', 
       owner: user, 
-      password: 'king1234', 
     )
   end
 
@@ -32,7 +30,6 @@ RSpec.describe Campaign, type: :model do
         image_url: 'https://cdnb.artstation.com/p/assets/images/images/023/999/093/large/gregory-nunkovics-dark-fantasy-landscape.jpg?1580994868',
         setting: 'Voyage to many track and fields never seen before...', 
         owner: user, 
-        password: 'king1234'
       )).to be_valid
     end
   end
@@ -42,16 +39,13 @@ RSpec.describe Campaign, type: :model do
     it { is_expected.to respond_to(:owner) }
     it { is_expected.to respond_to(:name) }
     it { is_expected.to respond_to(:setting) }
-    it { is_expected.to respond_to(:password_digest) }
   end
 
   describe 'validations' do
-    it { is_expected.to have_secure_password }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_most(30) }
     it { is_expected.to validate_uniqueness_of(:name).case_insensitive}
     it { is_expected.to validate_presence_of(:image_url) }
-    it { is_expected.to validate_length_of(:password).is_at_least(8) }
   end
 
   describe 'associations' do

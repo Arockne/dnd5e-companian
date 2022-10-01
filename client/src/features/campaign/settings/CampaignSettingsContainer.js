d01f0ad7b64be6379868eb27cebf1dc8bc6d2501
Daniel Calvin <arockne2003@yahoo.com>
2022-10-01 08:06:08 -0700

    remove CampaignUpdatePasswordForm

--- client/src/features/campaign/settings/CampaignSettingsContainer.js
+++ client/src/features/campaign/settings/CampaignSettingsContainer.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import CampaignDeleteContainer from './CampaignDeleteContainer'
 import CampaignUpdateForm from './CampaignUpdateForm'
-import CampaignUpdatePasswordForm from './CampaignUpdatePasswordForm'
 
 function CampaignSettingsContainer() {
   const { campaign, status, errors } = useSelector((state) => state.campaign)
@@ -11,8 +10,6 @@ function CampaignSettingsContainer() {
   return (
     <>
       <CampaignUpdateForm campaign={campaign} status={status} errors={errors} />
-      <Space h="lg" />
-      <CampaignUpdatePasswordForm campaign={campaign} />
       <Space h="lg" />
       <CampaignDeleteContainer campaign={campaign} status={status} />
     </>

706f2eb2fd500cbe0af1859a49dc1e6d8f176968
Daniel Calvin <arockne2003@yahoo.com>
2022-10-03 09:59:27 -0700

    move select form values to characterFormSelectValues

--- client/src/features/character/CharacterForm.js
+++ client/src/features/character/CharacterForm.js
@@ -17,6 +17,13 @@ import { useNavigate } from 'react-router-dom'
 import { client } from '../../api/client'
 import DialogPopUpErrors from '../error/DialogPopUpErrors'
 import CharacterOverview from './CharacterOverview'
+import {
+  races,
+  classes,
+  backgrounds,
+  alignments,
+  genders,
+} from './characterFormSelectValues'
 
 function CharacterForm() {
   const [form, setForm] = useState({
@@ -77,67 +84,6 @@ function CharacterForm() {
 
   const campaign = useSelector((state) => state.campaign.campaign)
 
-  const races = [
-    { value: 'Dwarf', label: 'Dwarf' },
-    { value: 'Elf', label: 'Elf' },
-    { value: 'Halfling', label: 'Halfling' },
-    { value: 'Human', label: 'Human' },
-    { value: 'Dragonborn', label: 'Dragonborn' },
-    { value: 'Gnome', label: 'Gnome' },
-    { value: 'Half-Elf', label: 'Half-Elf' },
-    { value: 'Half-Orc', label: 'Half-Orc' },
-    { value: 'Tiefling', label: 'Tiefling' },
-  ]
-
-  const klasss = [
-    { value: 'Barbarian', label: 'Barbarian' },
-    { value: 'Bard', label: 'Bard' },
-    { value: 'Cleric', label: 'Cleric' },
-    { value: 'Druid', label: 'Druid' },
-    { value: 'Fighter', label: 'Fighter' },
-    { value: 'Monk', label: 'Monk' },
-    { value: 'Paladin', label: 'Paladin' },
-    { value: 'Ranger', label: 'Ranger' },
-    { value: 'Rogue', label: 'Rogue' },
-    { value: 'Sorcerer', label: 'Sorcerer' },
-    { value: 'Warlock', label: 'Warlock' },
-    { value: 'Wizard', label: 'Wizard' },
-  ]
-
-  const backgrounds = [
-    { value: 'Acolyte', label: 'Acolyte' },
-    { value: 'Charlatan', label: 'Charlatan' },
-    { value: 'Criminal', label: 'Criminal' },
-    { value: 'Entertainer', label: 'Entertainer' },
-    { value: 'Folk Hero', label: 'Folk Hero' },
-    { value: 'Guild Artisan', label: 'Guild Artisan' },
-    { value: 'Hermit', label: 'Hermit' },
-    { value: 'Noble', label: 'Noble' },
-    { value: 'Outlander', label: 'Outlander' },
-    { value: 'Sage', label: 'Sage' },
-    { value: 'Sailor', label: 'Sailor' },
-    { value: 'Soldier', label: 'Soldier' },
-    { value: 'Urchin', label: 'Urchin' },
-  ]
-
-  const alignments = [
-    { value: 'Lawful Good', label: 'Lawful Good' },
-    { value: 'Neutral Good', label: 'Neutral Good' },
-    { value: 'Chaotic Good', label: 'Chaotic Good' },
-    { value: 'Lawful Neutral', label: 'Lawful Neutral' },
-    { value: 'Neutral', label: 'Neutral' },
-    { value: 'Chaotic Neutral', label: 'Chaotic Neutral' },
-    { value: 'Lawful Evil', label: 'Lawful Evil' },
-    { value: 'Neutral Evil', label: 'Neutral Evil' },
-    { value: 'Chaotic Evil', label: 'Chaotic Evil' },
-  ]
-
-  const genders = [
-    { value: 'Male', label: 'Male' },
-    { value: 'Female', label: 'Female' },
-    { value: 'Non-binary', label: 'Non-binary' },
-  ]
-
   function handleChange(e) {
     const { name, value } = e.target
     const updatedForm = { ...form, [name]: value }
@@ -190,7 +136,7 @@ function CharacterForm() {
             <Select
               label="Class"
               name="klass"
-              data={klasss}
+              data={classes}
               value={klass}
               onChange={setklass}
               searchable
---
+++ client/src/features/character/characterFormSelectValues.js
@@ -0,0 +1,62 @@
+const races = [
+  { value: 'Dwarf', label: 'Dwarf' },
+  { value: 'Elf', label: 'Elf' },
+  { value: 'Halfling', label: 'Halfling' },
+  { value: 'Human', label: 'Human' },
+  { value: 'Dragonborn', label: 'Dragonborn' },
+  { value: 'Gnome', label: 'Gnome' },
+  { value: 'Half-Elf', label: 'Half-Elf' },
+  { value: 'Half-Orc', label: 'Half-Orc' },
+  { value: 'Tiefling', label: 'Tiefling' },
+]
+
+const classes = [
+  { value: 'Barbarian', label: 'Barbarian' },
+  { value: 'Bard', label: 'Bard' },
+  { value: 'Cleric', label: 'Cleric' },
+  { value: 'Druid', label: 'Druid' },
+  { value: 'Fighter', label: 'Fighter' },
+  { value: 'Monk', label: 'Monk' },
+  { value: 'Paladin', label: 'Paladin' },
+  { value: 'Ranger', label: 'Ranger' },
+  { value: 'Rogue', label: 'Rogue' },
+  { value: 'Sorcerer', label: 'Sorcerer' },
+  { value: 'Warlock', label: 'Warlock' },
+  { value: 'Wizard', label: 'Wizard' },
+]
+
+const backgrounds = [
+  { value: 'Acolyte', label: 'Acolyte' },
+  { value: 'Charlatan', label: 'Charlatan' },
+  { value: 'Criminal', label: 'Criminal' },
+  { value: 'Entertainer', label: 'Entertainer' },
+  { value: 'Folk Hero', label: 'Folk Hero' },
+  { value: 'Guild Artisan', label: 'Guild Artisan' },
+  { value: 'Hermit', label: 'Hermit' },
+  { value: 'Noble', label: 'Noble' },
+  { value: 'Outlander', label: 'Outlander' },
+  { value: 'Sage', label: 'Sage' },
+  { value: 'Sailor', label: 'Sailor' },
+  { value: 'Soldier', label: 'Soldier' },
+  { value: 'Urchin', label: 'Urchin' },
+]
+
+const alignments = [
+  { value: 'Lawful Good', label: 'Lawful Good' },
+  { value: 'Neutral Good', label: 'Neutral Good' },
+  { value: 'Chaotic Good', label: 'Chaotic Good' },
+  { value: 'Lawful Neutral', label: 'Lawful Neutral' },
+  { value: 'Neutral', label: 'Neutral' },
+  { value: 'Chaotic Neutral', label: 'Chaotic Neutral' },
+  { value: 'Lawful Evil', label: 'Lawful Evil' },
+  { value: 'Neutral Evil', label: 'Neutral Evil' },
+  { value: 'Chaotic Evil', label: 'Chaotic Evil' },
+]
+
+const genders = [
+  { value: 'Male', label: 'Male' },
+  { value: 'Female', label: 'Female' },
+  { value: 'Non-binary', label: 'Non-binary' },
+]
+
+export { races, classes, backgrounds, alignments, genders }

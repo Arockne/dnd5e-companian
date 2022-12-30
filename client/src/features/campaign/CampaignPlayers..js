a1a9d0cf2d9e7edf1ba72e3337fda1fa46514ef6
Daniel Calvin <arockne2003@yahoo.com>
2022-12-30 10:32:23 -0700

    fetch players

--- client/src/features/campaign/CampaignPlayers..js
+++ client/src/features/campaign/CampaignPlayers..js
@@ -1,7 +1,18 @@
 import { Group, Title } from '@mantine/core'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function CampaignPlayers({ campaign }) {
+  const [players, setPlayers] = useState([])
+
+  useEffect(() => {
+    fetch(`/api/campaigns/${campaign.id}/players`).then(async (r) => {
+      const body = await r.json()
+      if (r.ok) {
+        setPlayers(body)
+      }
+    })
+  }, [])
+
   return (
     <div>
       <Group position="center">

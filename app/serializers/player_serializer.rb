84bf474476b44569d7a08bf506d72b9f28582718
Daniel Calvin <arockne2003@yahoo.com>
2022-12-31 13:56:35 -0700

    use options to filter characters based on campaign id

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -2,6 +2,7 @@ class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :username, :characters
 
   def characters
-    object.characters.select(:id, :name, :visible)
+    campaign_id = instance_options[:option_name][:campaign_id].to_i
+    object.characters.select(:id, :name, :visible).where(campaign_id: campaign_id)
   end
 end

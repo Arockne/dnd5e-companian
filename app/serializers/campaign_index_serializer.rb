3c29398d552efccdf59f9a01fcd8af93ac5aa224
Daniel Calvin <arockne2003@yahoo.com>
2023-01-03 11:03:05 -0700

    remove owner association

--- app/serializers/campaign_index_serializer.rb
+++ app/serializers/campaign_index_serializer.rb
@@ -1,8 +1,6 @@
 class CampaignIndexSerializer < ActiveModel::Serializer
   attributes :id, :name, :image_url, :setting
 
-  belongs_to :owner
-
   def setting
     "#{object.setting[0..75].strip}..."
   end

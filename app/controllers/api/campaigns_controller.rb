bda2b6afa28ad49a7ae251bbe7c34b6cee57a23b
Daniel Calvin <arockne2003@yahoo.com>
2023-01-03 17:11:10 -0700

    reorganize private methods

--- app/controllers/api/campaigns_controller.rb
+++ app/controllers/api/campaigns_controller.rb
@@ -64,6 +64,18 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def campaign_params
     params.require(:campaign).permit(:name, :setting, :image_url)
   end
+  
+  def membership
+    @membership ||= current_user.campaign_users.find_by(campaign_id: params[:id])
+  end
+  
+  def campaign
+    @campaign ||= Campaign.find(params[:id] || params[:campaign_id])
+  end
+  
+  def campaign_owner?
+    current_user == campaign.owner
+  end
 
   def render_unprocessable_entity(invalid)
     render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
@@ -71,18 +83,6 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   
   def render_not_found
     render json: { errors: ['The campaign you are looking for does not exist in this realm.'] }, status: :not_found
-  end
-
-  def membership
-    @membership ||= current_user.campaign_users.find_by(campaign_id: params[:id])
-  end
-
-  def campaign
-    @campaign ||= Campaign.find(params[:id] || params[:campaign_id])
-  end
-
-  def campaign_owner?
-    current_user == campaign.owner
   end
 
   def authorize_show_action
